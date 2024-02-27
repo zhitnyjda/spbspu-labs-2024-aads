f44ced12756018cd5d0096485ea18c59fc39c7ce
ElectronixTM <etdiary1@gmail.com>
2024-02-27 21:39:38 +0300

    started realising

--- miheev.egor/S1/list.hpp
+++ miheev.egor/S1/list.hpp
@@ -1,28 +1,51 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
-#include <utility>
+#include <cstddef>
 
 namespace miheev
 {
   template< typename T >
   class List
   {
-  public:
-    List(T data);
-    void push(T data);
-    void pop();
-
   private:
     T data_;
     List< T >* next_;
 
+  public:
+    class Iterator;
+    List(T data):
+      data_(data),
+      next_(nullptr)
+    {}
+    void push(T data)
+    {
+      if (next_ == nullptr)
+      {
+        next_ = new List< T >(data);
+      }
+      else
+      {
+        this->push(data);
+      }
+    }
+
+    Iterator begin()
+    {
+
+    }
+    Iterator end();
+
     class Iterator
     {
     private:
-      T* cur;
+      T* cur_;
 
     public:
+      Iterator(T* head)
+      {
+        cur_ = head;
+      }
       T& operator++();
       T& operator++(int);
       T& operator+(size_t);
--- miheev.egor/S1/main.cpp
+++ miheev.egor/S1/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <utility>
+#include <string>
+#include "list.hpp"
 
 int main()
 {
+  std::cout << "input string:\n";
+  std::string cur;
+  while (!std::cin.eof() && std::cin)
+  {
+    std::cin >> cur;
+    std::cout << cur << '\n';
+  }
   return 0;
 }
 
