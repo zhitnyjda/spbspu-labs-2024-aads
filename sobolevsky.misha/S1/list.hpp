1268bac355942b76f703e0e60ca2e928f36008cb
han7ter <sobmisha12@gmail.com>
2024-03-01 23:57:57 +0300

    пишу модификаторы

--- sobolevsky.misha/S1/list.hpp
+++ sobolevsky.misha/S1/list.hpp
@@ -1,16 +1,54 @@
 #ifndef LIST_HPP
 #define LIST_HPP
+#include "node.hpp"
 
 template< class T >
 class List
 {
   public:
-    List() = default;
-    void push();
+    List()
+    {
+      head = NULL;
+      tail = NULL;
+    }
+
+    Node * pushFront(T data)
+    {
+      Node * ptr = new Node(data);
+      ptr->next = head;
+      if (head != NULL)
+      {
+        head->prev = ptr;
+      }
+      if (tail == NULL)
+      {
+        tail = ptr;
+      }
+      head = ptr;
+      return ptr;
+    }
+
+    Node * pushBack(T data)
+    {
+      Node * ptr = new Node(data);
+      ptr->next = tail;
+      if (tail != NULL)
+      {
+        tail->prev = ptr;
+      }
+      if (head == NULL)
+      {
+        head = ptr;
+      }
+      tail = ptr;
+      return ptr;
+    }
     void pop();
     void clear();
     void swap();
   private:
+    Node * head;
+    Node * tail;
     size_t size;
 };
 
