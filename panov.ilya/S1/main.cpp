dc87755be4384c7d226cbc8ca779b9114fcb1427
ilyapanov123 <89517673116g@gmail.com>
2024-03-01 22:22:41 +0300

    pum pum

--- panov.ilya/S1/main.cpp
+++ panov.ilya/S1/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include <string>
 #include "list.hpp"
+
+/*
+#include "output" - пока не придумал 
+*/
 
 int main()
 {
-  using namespace Panov; 
+  using namespace Panov;
+  size_t size = 0;
+  size_t max_size_list = 0;
+ 
+/*try
+  {
+    ...
+  }
+  catch (const std::invalid_argument& e) - 
+  {
+    std::cerr << e.what() << '\n';
+    freeMemory();
+    return 1;
+  } */
 }
