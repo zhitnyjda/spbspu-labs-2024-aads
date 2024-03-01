ab19300a160598ff78da92937099b1d7fae83402
amotvei <motya2228@mail.ru>
2024-03-01 23:09:29 +0300

    third step, in mu hard lab

--- taskaev.matvei/S1/main.cpp
+++ taskaev.matvei/S1/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include "List.hpp"
+#include "ListIterator.hpp"
 
 int main()
 {
   using namespace taskaev;
+  List< std::pair< std::string, List < int > > > list;
+  std::string name;
+  List< int > nums;
+  std::cin >> name;
+  while(std::cin)
+  {
+    list.pushFront({name, nums});
+    while (std::cin >> name && std::isdigit(name[0]))
+    {
+      list.head->data.second.pushFront(std::stoll(name));
+    }
+  }
+  list.reverse();
+  ListIterator< std::pair< std::string, List< int > > > i = list.begin();
+  while(i != list.end())
+  {
+   std::cout << i.node->data.first << " ";
+    ++i;
+  }
+  std::cout << "\n";
   return 0;
 }
