9a7361f5045f74e01e95508c54ea3f8a23baace3
0412erik <litsinger00@bk.ru>
2024-03-02 00:01:13 +0300

    update main

--- litsinger.ernest/S1/main.cpp
+++ litsinger.ernest/S1/main.cpp
@@ -6,20 +6,20 @@
 
 int main()
 {
-	//litsinger::List< int > listik;
-	//std::string str = "";
-	//char temp = 0;
-	//std::cin >> std::noskipws;
-	//while (!std::cin.eof())
+  //litsinger::List< int > listik;
+  //std::string str = "";
+  //char temp = 0;
+  //std::cin >> std::noskipws;
+  //while (!std::cin.eof())
+  //{
+	//std::cin >> temp;
+	//if (temp == '\n')
 	//{
-		//std::cin >> temp;
-		//if (temp == '\n')
-		//{
-			//std::cout << str << "\n";
-			//str = "";
-		//}
-		//str += temp;
+	  //std::cout << str << "\n";
+	  //str = "";
 	//}
-	//std::cin >> std::skipws;
-	//return 0;
+    //str += temp;	
+  //}
+  //std::cin >> std::skipws;
+  //return 0;
 }
