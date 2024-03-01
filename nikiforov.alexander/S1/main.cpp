51611734e4020b32fd0ca12c4ea88fef30e85e7c
AlexNik <sahan@DESKTOP-R8OTJOJ>
2024-03-01 16:39:19 +0300

    Reapply "starting"
    This reverts commit 14945119ec6e9f3bcf40b69489e7bfb1b442168f.

---
+++ nikiforov.alexander/S1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstddef>
+#include "list.hpp"
+
+
+int main()
+{
+	List<int> lst;
+	bool empty = lst.is_empty();
+	lst.push_back(10);
+	lst.push_back(4);
+	lst.push_back(9);
+	lst.push_back(1);
+	
+
+	return 0;
+}
+
+
