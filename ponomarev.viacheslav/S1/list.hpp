1bebf4a1af841c7f53597ca85856227a0d1ec682
ponomarev.viacheslav <slava_ponomarev_05@mail.ru>
2024-03-01 22:29:35 +0300

    Added class ConstIterator

--- ponomarev.viacheslav/S1/list.hpp
+++ ponomarev.viacheslav/S1/list.hpp
@@ -6,6 +6,67 @@ namespace ponomarev
   template< typename T >
   class List
   {
+    public:
+      class ConstIterator
+      {
+        private:
+          explicit ConstIterator(const Node * ptr) noexcept:
+            elem { ptr } {}
+        public:
+          T & operator*() const noexcept
+          {
+            assert(elem != nullptr);
+            return elem->data;
+          }
+
+          ConstIterator & operator++() noexcept
+          {
+            assert(elem != nullptr);
+            elem = elem->next;
+            return *this;
+          }
+
+          ConstIterator & operator--() noexcept
+          {
+            assert(elem != nullptr);
+            elem = elem->prev;
+            return *this;
+          }
+
+          ConstIterator operator++(int) noexcept
+          {
+            assert(elem != nullptr);
+            auto copy = *this;
+            elem = elem->next;
+            return copy;
+          }
+
+          ConstIterator operator--() noexcept
+          {
+            assert(elem != nullptr);
+            auto copy = *this;
+            elem = elem->prev;
+            return copy;
+          }
+
+          bool operator==(ConstIterator other) const noexcept
+          {
+            return elem == other.elem;
+          }
+
+          bool operator!=(ConstIterator other) const noexcept
+          {
+            return !(*this == other);
+          }
+        protected:
+          const Node *get() const noexcept
+          {
+            return elem;
+          }
+
+          const Node * elem;
+      };
+
     private:
       struct Node
       {
