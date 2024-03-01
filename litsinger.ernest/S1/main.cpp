7aacdc36ccb98c180e8c0ae746657ab44e206b53
0412erik <litsinger00@bk.ru>
2024-03-01 23:51:47 +0300

    add main

---
+++ litsinger.ernest/S1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "list.hpp"
+
+int main()
+{
+
+	//litsinger::List< int > listik;
+	//std::string str = "";
+	//char temp = 0;
+	//std::cin >> std::noskipws;
+	//while (!std::cin.eof())
+	//{
+		//std::cin >> temp;
+		//if (temp == '\n')
+		//{
+			//std::cout << str << "\n";
+			//str = "";
+		//}
+		//str += temp;
+	//}
+	//std::cin >> std::skipws;
+	return 0;
+}
