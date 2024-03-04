d6d52a4df0b3e6e3c3a5f0add3d959549ab72225
Александр Псарев <alexandrpsarev5555@gmail.com>
2024-03-04 23:36:41 +0500

    completed task

--- psarev.alexandr/S1/main.cpp
+++ psarev.alexandr/S1/main.cpp
@@ -6,36 +6,85 @@
 int main()
 {
   using namespace psarev;
-  List< std::pair< std::string, List< int > > > seqs;
 
-  seqs.pushBack({ "", {} });
-  Iterator< std::pair< std::string, List< int > > > iterSeqs = seqs.begin();
-  (*iterSeqs).second.pushBack(14);
-  Iterator< int > iter = (*iterSeqs).second.begin();
-
-  std::cout << *iter << '\n';
-//testing ^
-//-------------------------------------------
-//variant
   List< std::pair< std::string, List< int > > > lines;
-  Iterator< std::pair< std::string, List< int > > > iterLine = seqs.begin();
-  iterLine--;
+  Iterator< std::pair< std::string, List< int > > > iterLine;
   std::string line = "";
-  while (std::cin >> line)
+  size_t maxDigits = 0;
+  while (std::getline(std::cin, line))
   {
     if (line.size() != 0)
     {
       std::string ord = psarev::getOrd(line);
       lines.pushBack({ ord, {} });
-      iterLine++;
-
+      iterLine = lines.begin();
+      for (size_t i = 0; i < (lines.getSize() - 1); i++)
+      {
+        iterLine++;
+      }
+      size_t cntDigits = 0;
       while (line.size() != 0)
       {
         int digit = psarev::getDigit(line);
         (*iterLine).second.pushBack(digit);
+        cntDigits++;
       }
+      maxDigits = fmax(maxDigits, cntDigits);
     }
   }
+  iterLine = lines.begin();
+  for (size_t i = 0; i < lines.getSize(); i++)
+  {
+    std::cout << (*iterLine).first << ' ';
+    iterLine++;
+  }
+  std::cout << '\n';
+  Iterator< int > iterDig;
+  List< int > sums;
+  int curSum = 0;
+  for (size_t i = 0; i < maxDigits; i++)
+  {
+    curSum = 0;
+    iterLine = lines.begin();
+    for (size_t j = 0; j < lines.getSize(); j++)
+    {
+      iterDig = (*iterLine).second.begin();
+      if ((*iterLine).second.getSize() > i)
+      {
+        for (size_t k = 0; k < i; k++)
+        {
+          iterDig++;
+        }
+        if ((j == lines.getSize()) && (i == maxDigits))
+        {
+          std::cout << *iterDig;
+        }
+        else
+        {
+          std::cout << *iterDig << ' ';
+        }
+        curSum += *iterDig;
+      }
+      iterLine++;
+    }
+    sums.pushBack(curSum);
+    std::cout << '\n';
+  }
+
+  Iterator< int > iterSums = sums.begin();
+  for (size_t i = 0; i < sums.getSize(); i++)
+  {
+    if (i == sums.getSize())
+    {
+      std::cout << *iterSums;
+    }
+    else
+    {
+      std::cout << *iterSums << ' ';
+    }
+    iterSums++;
+  }
+  std::cout << '\n';
 
   return 0;
 }
