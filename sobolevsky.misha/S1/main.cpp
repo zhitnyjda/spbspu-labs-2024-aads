b9e356335ca4f52bc6b1c10c4890b74b978a867c
han7ter <sobmisha12@gmail.com>
2024-03-14 00:38:56 +0300

    ну хотя бы один тест пройти должен

--- sobolevsky.misha/S1/main.cpp
+++ sobolevsky.misha/S1/main.cpp
@@ -9,6 +9,7 @@ int main()
 
   size_t maxlen = 0;
   size_t currlen = 0;
+  size_t length = 0;
   std::string elem;
   while (std::cin >> elem)
   {
@@ -22,6 +23,7 @@ int main()
       std::vector< int > vec;
       currlen = 0;
       list.pushBack(vec, elem);
+      length++;
     }
     else
     {
@@ -31,8 +33,50 @@ int main()
     }
   }
 
-  std::cout << list.head->name << "\n";
-  std::cout << list.head->data[0] << "\n";
+  sobolevsky::Iterator< std::vector< int > > iter(list.head);
+  for (size_t i = 0; i < length; i++)
+  {
+    std::cout << iter.name();
+    ++iter;
+    if (i != (length - 1))
+    {
+      std::cout << " ";
+    }
+  }
+  std::cout << "\n";
+
+  std::vector< int > lastList;
+  size_t sumInts = 0;
+  for (size_t i = 0; i < maxlen; i++)
+  {
+    sumInts = 0;
+    sobolevsky::Iterator< std::vector< int > > iter2(list.head);
+    for (size_t j = 0; j < length; j++)
+    {
+      if (i < iter2.currNode->data.size())
+      {
+        std::cout << iter2.currNode->data[i];
+        sumInts += iter2.currNode->data[i];
+        if (j != (length - 1))
+        {
+          std::cout << " ";
+        }
+      }
+      ++iter2;
+    }
+    lastList.push_back(sumInts);
+    std::cout << "\n";
+  }
+
+  for (size_t i = 0; i < lastList.size(); i++)
+  {
+    std::cout << lastList[i];
+    if (i != (lastList.size() - 1))
+    {
+      std::cout << " ";
+    }
+  }
+  std::cout << "\n";
 
   return 0;
 }
