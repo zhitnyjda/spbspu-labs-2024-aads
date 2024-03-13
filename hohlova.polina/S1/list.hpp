3af8f18fe46410d79e1fb74d73bafb1ce48a947f
phohlova <pv.hohlova@edu.spbstu.ru>
2024-03-13 18:41:17 +0300

    -

--- hohlova.polina/S1/list.hpp
+++ hohlova.polina/S1/list.hpp
@@ -1,7 +1,9 @@
+#ifndef LIST_HPP
+#define LIST_HPP
 #include <iostream>
-#include <algorithm>
+#include <sstream>
+#include <vector>
 #include <string>
-
 
 namespace hohlova
 {
@@ -12,12 +14,15 @@ namespace hohlova
     List();
     ~List();
     void pop_front();
-    void push_back(T data);
+    void push_back(const std::vector<T>& nums);
     void clear();
-    int getSize() { return size; }
-    T& operator[](const int index);
-    void push_front(T data);
-    void insert(T data, int index);
+    size_t size() const;
+
+    const std::vector<T>& operator[](size_t index) const { return data[index]; }
+    std::vector<T>& operator[](size_t index) { return data[index]; }
+
+    void push_front(std::vector<std::vector<T>> data);
+    void insert(std::vector<std::vector<T>> data, int index);
     void removeAt(int index);
     void pop_back();
   private:
@@ -33,128 +38,107 @@ namespace hohlova
         this->nextPoint = nextPoint;
       }
     };
-    int size;
+    std::vector<std::vector<T>> data;
     Node<T>* head;
+    int Size = 0;
   };
+}
 
-  template<typename T>
-  List<T>::List()
+template<typename T>
+List<T>::List()
+{
+  int Size = 0;
+  Node<T>* head = nullptr;
+}
+
+template<typename T>
+List<T>::~List()
+{
+  clear();
+}
+
+template<typename T>
+void List<T>::pop_front()
+{
+  Node<T>* temp = head;
+  head = head->nextPoint;
+  delete temp;
+  Size--;
+}
+
+template<typename T>
+void List<T>::push_back(const std::vector<T>& nums)
+{
+  data.push_back(nums);
+}
+
+template<typename T>
+void List<T>::clear()
+{
+  while (Size)
   {
-    int size = 0;
-    Node<T>* head = nullptr;
-  }
-  template<typename T>
-  List<T>::~List()
-  {
-    clear();
-  }
-
-  template<typename T>
-  void List<T>::pop_front()
-  {
-    Node<T>* temp = head;
-    head = head->nextPoint;
-    delete temp;
-    size--;
-  }
-
-  template<typename T>
-  void List<T>::push_back(T data)
-  {
-    if (head == nullptr)
-    {
-      head = new Node<T>(data);
-    }
-    else
-    {
-      Node<T>* currentPoint = this->head;
-
-      while (currentPoint->nextPoint != nullptr)
-      {
-        currentPoint = currentPoint->nextPoint;
-      }
-      currentPoint->nextPoint = new Node<T>(data);
-
-    }
-    size++;
-  }
-
-  template<typename T>
-  void List<T>::clear()
-  {
-    while (size)
-    {
-      pop_front();
-    }
-  }
-
-  template<typename T>
-  T& List<T>::operator[](const int index)
-  {
-    int count = 0;
-    Node<T>* currentPoint = this->head;
-    while (currentPoint != nullptr)
-    {
-      if (count == index)
-      {
-        return currentPoint->data;
-      }
-      currentPoint = currentPoint->nextPoint;
-      count++;
-    }
-  }
-
-  template<typename T>
-  void List<T>::push_front(T data)
-  {
-    head = new Node<T>(data, head);
-    size++;
-  }
-
-  template<typename T>
-  void List<T>::insert(T data, int index)
-  {
-    if (index == 0)
-    {
-      push_front(data);
-    }
-    else
-    {
-      Node<T>* prevPoint = this->head;
-      for (int i = 0; i < index - 1; i++)
-      {
-        prevPoint = prevPoint->nextPoint;
-      }
-      Node<T>* newNode = new Node<T>(data, prevPoint->nextPoint);
-      prevPoint->nextPoint = newNode;
-      size++;
-    }
-  }
-
-  template<typename T>
-  void List<T>::removeAt(int index)
-  {
-    if (index == 0)
-    {
-      pop_front();
-    }
-    else
-    {
-      Node<T>* prevPoint = this->head;
-      for (int i = 0; i < index - 1; i++)
-      {
-        prevPoint = prevPoint->nextPoint;
-      }
-      Node<T>* forDelete = prevPoint->nextPoint;
-      prevPoint->nextPoint = forDelete->nextPoint;
-      delete forDelete;
-      size--;
-    }
-  }
-
-  template<typename T>
-  void List<T>::pop_back()
-  {
-    removeAt(size - 1);
+    pop_front();
   }
 }
+
+template <typename T>
+size_t hohlova::List<T>::size() const
+{
+  return data.size();
+}
+
+template<typename T>
+void List<T>::push_front(std::vector<std::vector<T>> data)
+{
+  head = new Node<T>(data, head);
+  Size++;
+}
+
+template<typename T>
+void List<T>::insert(std::vector<std::vector<T>> data, int index)
+{
+  if (index == 0)
+  {
+    push_front(data);
+  }
+  else
+  {
+    Node<T>* prevPoint = this->head;
+    for (int i = 0; i < index - 1; i++)
+    {
+      prevPoint = prevPoint->nextPoint;
+    }
+    Node<T>* newNode = new Node<T>(data, prevPoint->nextPoint);
+    prevPoint->nextPoint = newNode;
+    Size++;
+  }
+}
+
+template<typename T>
+void List<T>::removeAt(int index)
+{
+  if (index == 0)
+  {
+    pop_front();
+  }
+  else
+  {
+    Node<T>* prevPoint = this->head;
+    for (int i = 0; i < index - 1; i++)
+    {
+      prevPoint = prevPoint->nextPoint;
+    }
+    Node<T>* forDelete = prevPoint->nextPoint;
+    prevPoint->nextPoint = forDelete->nextPoint;
+    delete forDelete;
+    Size--;
+  }
+}
+
+template<typename T>
+void List<T>::pop_back()
+{
+  removeAt(Size - 1);
+}
+
+#endif
--- hohlova.polina/S1/main.cpp
+++ hohlova.polina/S1/main.cpp
@@ -1,26 +1,69 @@
-#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
 #include "list.hpp"
 
-int main()
+
+int main() 
 {
-  hohlova::List<std::pair<std::string, hohlova::List<int>>> seq;
-  std::string name;
-  hohlova::List<int> nums;
-  while (std::cin)
-  {
-    seq.push_front({ name, nums });
-    while (std::cin >> name && std::isdigit(name[0]))
+    hohlova::List<int> numbers;
+    std::vector<std::string> words;
+    std::string line;
+
+    while (std::getline(std::cin, line) && !line.empty()) 
     {
-      seq.push_front({name, nums});
+        std::istringstream iss(line);
+        std::string word;
+        iss >> word;
+        words.push_back(word);
+
+        std::vector<int> nums;
+        int num;
+        while (iss >> num) 
+        {
+            nums.push_back(num);
+        }
+        numbers.push_back(nums);
     }
-  }
-  if (!std::cin)
-  {
-    for (int i = 0; i < seq.getSize(); i++)
+
+    size_t max_length = 0;
+    for (size_t i = 0; i < numbers.size(); ++i) 
     {
-      std::cout << nums[i] << std::endl;
+        max_length = std::max(max_length, numbers[i].size());
     }
+
+    for (const auto& word : words) 
+    {
+        std::cout << word << " ";
+    }
+    std::cout << "\n";
+
+    for (size_t i = 0; i < max_length; ++i) 
+    {
+      for (size_t j = 0; j < numbers.size(); ++j) 
+      {
+        if (i < numbers[j].size()) 
+        {
+          std::cout << numbers[j][i] << " ";
+        }
+      }
+    std::cout << "\n";
   }
+
+  for (size_t i = 0; i < max_length; ++i) 
+  {
+    int sum = 0;
+    for (size_t j = 0; j < numbers.size(); ++j) 
+    {
+      if (i < numbers[j].size()) 
+      {
+        sum += numbers[j][i];
+      }
+    }
+    std::cout << sum << " ";
+  }
+  std::cout << "\n";
+
   return 0;
 }
-
