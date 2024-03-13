60548f469003cf66f4bb36c218bed70773e29ccb
urtanto <anikanovvlad@yandex.ru>
2024-03-13 23:35:35 +0300

    fin pls

--- anikanov.vladislav/S1/main.cpp
+++ anikanov.vladislav/S1/main.cpp
@@ -9,10 +9,9 @@ int main()
 {
   using namespace anikanov;
 
-  std::unique_ptr< List< std::pair< char[51], List< int > > > > lists(new List< std::pair< char[51], List< int > > >);
+  std::unique_ptr< List< std::pair< char[51], List< long long > > > > lists(new List< std::pair< char[51], List< long long > > >);
   char input_name[51];
-  int number;
-  bool bad_input = false;
+  long long number;
 
   while (true) {
 
@@ -20,19 +19,18 @@ int main()
       break;
     }
 
-    List< int > numbers;
+    List< long long > numbers;
     while (std::cin.peek() != '\n') {
       if (std::cin >> number) {
         numbers.push_back(number);
       } else {
-        bad_input = true;
         std::cin.clear();
         std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
         break;
       }
     }
 
-    std::pair< char[51], List< int > > list_pair;
+    std::pair< char[51], List< long long > > list_pair;
     std::strcpy(list_pair.first, input_name);
     list_pair.second = numbers;
     lists->push_back(list_pair);
@@ -48,11 +46,6 @@ int main()
     }
   }
 
-  if (bad_input) {
-    std::cerr << "overflow\n";
-    return 1;
-  }
-
   if (lists->empty()) {
     std::cout << "0\n";
     return 0;
@@ -63,14 +56,18 @@ int main()
   bool finished = false;
   bool start_print = false;
   size_t idx = 0;
-  List< int > sums;
+  List< long long > sums;
+  bool bad_sum = false;
   while (!finished) {
     finished = true;
     start_print = true;
-    sums.push_back(0);
     for (auto list_it = lists->begin(); list_it != lists->end(); ++list_it) {
       const auto &list = *list_it;
       if (idx < list.second.size()) {
+        if (finished){
+          sums.push_back(0);
+          finished = false;
+        }
         auto num_it = list.second.begin();
         for (size_t i = 0; i < idx; ++i) {
           num_it++;
@@ -81,19 +78,31 @@ int main()
         } else {
           std::cout << " " << *num_it;
         }
+        if (*sums.back() + *num_it > std::numeric_limits< long long >::max()) {
+          std::cerr << "overflow\n";
+          bad_sum = true;
+        }
         *sums.back() += *num_it;
-        finished = false;
       }
     }
     std::cout << (finished ? "" : "\n");
     idx++;
   }
-  sums.pop();
+
+  if (bad_sum) {
+    std::cerr << "overflow\n";
+    return 1;
+  }
+
   for (auto sum_it = sums.begin(); sum_it != sums.end();) {
     std::cout << *sum_it;
     if (++sum_it != sums.end()) {
       std::cout << " ";
     }
+  }
+
+  if (sums.empty()){
+    std::cout << "0";
   }
 
   std::cout << "\n";
