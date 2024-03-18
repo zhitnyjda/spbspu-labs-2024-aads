f81400bf9168f59b9fed20c127d57324fd00a555
unknown <iulia.lis1tsyna@yandex.ru>
2024-03-18 18:45:34 +0300

    complete modification of the list file

--- lisitsyna.yulia/S1/list.hpp
+++ lisitsyna.yulia/S1/list.hpp
@@ -1,118 +1,54 @@
 #ifndef LIST_HPP
 #define LIST_HPP
-#include "node.hpp"
-#include "iterator.hpp"
 #include <cstddef>
-#include <memory>
+#include <iterator>
+#include <stdexcept>
 
-namespace lisitsyna
+template<typename T>
+class ListNode
 {
-  template<typename T>
-  class List
+public:
+  T value;
+  ListNode* next;
+  ListNode(T val) : value(val), next(nullptr) {}
+};
+template<typename T>
+class List
+{
+public:
+  ListNode<T>* head;
+  ListNode<T>* tail;
+  size_t size;
+
+  List() : head(nullptr), tail(nullptr), size(0) {}
+  List(const List& other) : head(nullptr), tail(nullptr), size(0)
   {
-  public:
-    Node<T>* head;
-    Node<T>* tail;
-    size_t size;
-    List() : head(nullptr), tail(nullptr), size(0) {}
-    ~List()
+    ListNode<T>* current = other.head;
+    while (current != nullptr)
     {
-      clear();
+      push_back(current->value);
+      current = current->next;
     }
-
-    T& operator[](const size_t index);
-    void popFront();
-    void pushBack(T& data);
-    void clear();
-    void pushFront(const T& data);
-    void popBack();
-    size_t GetSize();
-    bool isEmpty();
-
-  private:
-    Node<T>* head, tail;
-    size_t size() const;
-  };
-}
-
-template<typename T>
-lisitsyna::List<T>::~List()
-{
-  clearList();
-}
-
-template<typename T>
-void lisitsyna::List<T>::popFront()
-{
-  if (head == nullptr) return;
-  if (head == tail)
-  {
-    delete tail;
-    head = tail = nullptr;
-    return;
   }
-
-  Node<T>* node = head
-  head = node->next;
-  delete node;
-}
-
-template<typename T>
-void lisitsyna::List<T>::pushBack(T data)
-{
-  Node<T>* node = new Node(data);
-  if (head == nullptr) head = node;
-  if (tail != nullptr) tail->next = node;
-  tail = node;
-}
-
-template<typename T>
-void lisitsyna::List<T>::clearList()
-{
-  while (size)
+  List(List&& other) noexcept : head(other.head), tail(other.tail), size(other.size)
   {
-    popFront();
+    other.head = other.tail = nullptr;
+    other.size = 0;
   }
-}
-
-template<typename T>
-T& lisitsyna::List<T>::operator[](const size_t index)
-{
-  if (index >= size) throw std::out_of_range("Index out of bounds");
-  Node<T>* current = head;
-  for (size_t i = 0; i < index; i++)
+  List& operator=(List other)
   {
-    current = current->next;
+    swap(*this, other);
+    return *this;
   }
-  return current->data;
-}
+  ~List() { clear(); }
 
-template<typename T>
-void lisitsyna::List<T>::pushFront(T& data)
-{
-  Node<T>* node = new Node(data);
-  node->next = head;
-  head = node;
-  if (tail == nullptr) tail = node;
-}
-
-template<typename T>
-void lisitsina::List<T>::popBack(T data)
-{
-  if (tail == nullptr) return;
-  if (head == tail)
+  void push_back(T val)
   {
-    delete tail;
-    head = tail = nullptr;
-    return;
+        ListNode<T>* newNode = new ListNode<T>(val);
+        if (tail) tail->next = newNode;
+        else head = newNode;
+        tail = newNode;
+        ++size;
   }
-  Node<T>* node = head;
-  for(node->next != != tail; node = node->next);
-  node->next = nullptr;
-  delete tail;
-  tail = node;
-}
-
-
 }
 #endif
