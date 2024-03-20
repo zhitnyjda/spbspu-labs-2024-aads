190e24efcc6cebd307796fd9ff8776c818f5489a
harduserNIKITA <cumpotttt@yandex.ru>
2024-03-20 20:08:22 +0300

    check build

--- kovshikov.nikita/S1/main.cpp
+++ kovshikov.nikita/S1/main.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
 #include "doublelist.hpp"
-#include <utility>
+#include <string>
 
 using namespace kovshikov;
 int main()
 {
-  std::cout << "start checking DoubleList" << "\n";
-  DoubleList<int> list;
-  list.pushFront(3);
-  list.pushFront(4);
-  list.pushFront(7);
-  list.remove(4);
-  list.popFront();
-  std::cout << list.front() << "\n";
-  list.remove(4);
-  std::cout << list.empty() << "\n";
-  list.remove(3);
-  std::cout << list.empty() << "\n";
-  list.pushFront(2);
-  list.pushFront(9);
-  list.remove(9);
-  std::cout << list.front() << "\n";
-  return 0;
+  DoubleList<std::pair<std::string, int>> allPairs;
+  std::string str = "nikita";
+  int num = 9;
+  allPairs.pushBack({str, num});
+ // std::cout << allPairs.front().second << "\n";
+
+
+
+
+
+  //сохранить все последовательности в allPairs;
+
+
+ return 0;
 }
