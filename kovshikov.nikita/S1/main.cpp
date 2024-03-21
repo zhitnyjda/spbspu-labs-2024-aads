9bb834b2d172cea1ad1e78abc2a3cd2e86ab39d5
harduserNIKITA <cumpotttt@yandex.ru>
2024-03-21 15:05:15 +0300

    attemt to add input

--- kovshikov.nikita/S1/main.cpp
+++ kovshikov.nikita/S1/main.cpp
@@ -1,22 +1,70 @@
 #include <iostream>
 #include "doublelist.hpp"
+#include <utility>
 #include <string>
 
 using namespace kovshikov;
 int main()
 {
-  DoubleList<std::pair<std::string, int>> allPairs;
-  std::string str = "nikita";
-  int num = 9;
-  allPairs.pushBack({str, num});
- // std::cout << allPairs.front().second << "\n";
+ /* std::string string = " 2 52 2 2 32";
+  std::cout << string[0] << "\n";
+  std::cout << string.length() << "\n";
+  std::string tempString = "";
+  for(size_t i = 0; i < string.length(); i++)
+  {
+    if(string[i] != ' ')
+    {
+      tempString.append(1, string[i]);
+    }
+    else
+    {
+      if(!tempString.empty())
+      {
+        int integer = std::stoi(tempString, nullptr, 10);
+        std::cout << integer << "\n";
+        tempString.clear();
+      }
+    }
+  }
+  int integer = std::stoi(tempString, nullptr, 10);
+  std::cout << integer << "\n";*/
 
-
-
-
-
-  //сохранить все последовательности в allPairs;
-
-
- return 0;
+  DoubleList<std::pair<std::string, DoubleList<int>>> allPairs; //список всех пар
+  int i = 0;
+  while(!std::cin.eof()) // производится ввод, до момента ctrl + D
+  {
+    i++;
+    std::cout << "pair number: " << i << "\n";
+    DoubleList<int> list;
+    std::string listName;
+    std::string integerString;
+    std::cin >> listName; //запишется наименование последовательности
+    std::getline(std::cin, integerString); //вводятся числа в качестве строки
+    if(!listName.empty())
+    {
+      std::string tempString;
+      int integer;
+      for(size_t i = 0; i < integerString.length(); i++)
+      {
+        if(integerString[i] != ' ')
+        {
+          tempString.append(1, integerString[i]);
+        }
+        else
+        {
+          if(!tempString.empty())
+          {
+            integer = std::stoi(tempString, nullptr, 10);
+            list.pushBack(integer);
+            tempString.clear();
+          }
+        }
+      }
+      integer = std::stoi(tempString, nullptr, 10); //проверка на пустоту строки
+      list.pushBack(integer);
+      std::pair<std::string, DoubleList<int>> element = {listName, list};
+      allPairs.pushBack(element);
+    }
+  }
+  return 0;
 }
