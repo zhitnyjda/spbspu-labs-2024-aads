a1e9ba53782494dea68ab5a9013b13225614e47e
anyadg <anna_giraffe@mail.ru>
2024-03-24 01:11:38 +0300

    вынесение вывода в отдельную функцию

--- doroshenko.anna/S1/functions.cpp
+++ doroshenko.anna/S1/functions.cpp
@@ -29,6 +29,89 @@ doroshenko::List< std::pair< std::string, doroshenko::List< size_t > > > doroshe
   return inputList;
 }
 
+void doroshenko::listOutput(doroshenko::List< std::pair< std::string, doroshenko::List< size_t > > > list, std::ostream& output)
+{
+  ListIterator<std::pair< std::string, List< size_t > > > iterator = list.begin();
+  size_t maxSize = getMaxSize(list);
+  List< size_t > sums;
+  ListIterator< size_t > iteratorForSums;
+  const size_t maximum = std::numeric_limits< size_t >::max();
+  size_t sum = 0;
+  bool overflowError = false;
+  if (!list.isEmpty())
+  {
+    while (iterator.node)
+    {
+      if (iterator.node != list.head_)
+      {
+        output << " ";
+      }
+      output << iterator.node->data.first;
+      iterator++;
+    }
+    output << "\n";
+    for (size_t index = 0; index < maxSize; index++)
+    {
+      iterator = list.begin();
+      while (iterator.node)
+      {
+        if (iterator.node->data.second[index] != nullptr)
+        {
+          if (sum > 0)
+          {
+            output << " ";
+          }
+          std::cout << iterator.node->data.second[index]->data;
+          if (maximum - sum < iterator.node->data.second[index]->data)
+          {
+            overflowError = true;
+            output << "\n";
+            throw std::overflow_error("overflow error");
+          }
+          else
+          {
+            sum = sum + iterator.node->data.second[index]->data;
+          }
+        }
+        iterator++;
+      }
+      if (!overflowError)
+      {
+        sums.pushBack(sum);
+      }
+      sum = 0;
+      overflowError = false;
+      output << "\n";
+    }
+
+    iteratorForSums = sums.begin();
+    if (sums.isEmpty())
+    {
+      output << 0;
+    }
+    else
+    {
+      while (iteratorForSums.node)
+      {
+        if (iteratorForSums.node != nullptr)
+        {
+          if (iteratorForSums.node != sums.head_)
+          {
+            output << " ";
+          }
+          output << iteratorForSums.node->data;
+        }
+        iteratorForSums++;
+      }
+    }
+    output << "\n";
+  }
+  else
+  {
+    output << 0 << "\n";
+  }
+}
+
 size_t doroshenko::getMaxSize(doroshenko::List< std::pair< std::string, doroshenko::List< size_t > > >& list)
 {
   size_t size = 0;
--- doroshenko.anna/S1/functions.hpp
+++ doroshenko.anna/S1/functions.hpp
@@ -8,6 +8,7 @@ namespace doroshenko
 {
   List< std::pair< std::string, List< size_t > > > listInput(std::istream& input);
   size_t getMaxSize(List< std::pair< std::string, List< size_t > > >& list);
+  void listOutput(List< std::pair< std::string, List< size_t > > >, std::ostream& output);
 }
 
 #endif
