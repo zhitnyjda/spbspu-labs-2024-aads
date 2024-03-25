52b859af439a801b3bd85337de0f69e9f05ba6d3
anyadg <anna_giraffe@mail.ru>
2024-03-25 21:33:11 +0300

    trying to fix sermentation fault

--- doroshenko.anna/S1/functions.cpp
+++ doroshenko.anna/S1/functions.cpp
@@ -119,7 +119,7 @@ size_t doroshenko::getMaxSize(doroshenko::List< std::pair< std::string, doroshen
   size_t size = 0;
   size_t maxSize = 0;
   List< std::pair< std::string, doroshenko::List< size_t > > >::Iterator iterator = list.begin();
-  while (iterator != list.end())
+  while (iterator != nullptr)
   {
     size = iterator->second.getSize();
     maxSize = size > maxSize ? size : maxSize;
