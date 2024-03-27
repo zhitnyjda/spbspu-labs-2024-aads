62b000d5bba08fb09b010f1c529150e2678eae40
phohlova <pv.hohlova@edu.spbstu.ru>
2024-03-27 18:29:15 +0300

    acceptance

--- hohlova.polina/S1/main.cpp
+++ hohlova.polina/S1/main.cpp
@@ -30,97 +30,98 @@ int main()
     if (!nums.empty())
       hasData = true;
   }
-  if (!hasData)
-  {
-    std::cout << temp << std::endl
-      << "0\n";
-    return 0;
-  }
 
   if (numbers.size() == 0)
   {
-    std::cout << "0" << std::endl;
-    return 0;
+    std::cout << 0 << '\n';
   }
-
-  size_t max_length = 0;
-  for (auto it = numbers.begin(); it != numbers.end(); ++it)
+  else
   {
-    max_length = std::max(max_length, (*it).second.size());
-  }
-
-  for (auto it = numbers.begin(); it != numbers.end(); ++it)
-  {
-    std::cout << (*it).first;
-    auto nextIt = it;
-    ++nextIt;
-    if (nextIt != numbers.end())
+    if (!hasData)
     {
-      std::cout << " ";
+      std::cout << temp << "\n" << 0 << "\n";
+      return 0;
     }
-  }
-  std::cout << std::endl;
-
-  size_t max_size = 0;
-  for (const auto& pair : numbers)
-  {
-    const auto& sequence = pair.second;
-    max_size = std::max(max_size, sequence.size());
-  }
-  for (size_t i = 0; i < max_size; ++i)
-  {
-    bool firstElement = true;
-    for (const auto& pair : numbers)
+    size_t max_length = 0;
+    for (auto it = numbers.begin(); it != numbers.end(); ++it)
     {
-      const auto& sequence = pair.second;
-      if (i < sequence.size())
+      max_length = std::max(max_length, (*it).second.size());
+    }
+
+    for (auto it = numbers.begin(); it != numbers.end(); ++it)
+    {
+      std::cout << (*it).first;
+      auto nextIt = it;
+      ++nextIt;
+      if (nextIt != numbers.end())
       {
-        if (!firstElement)
-        {
-          std::cout << " ";
-        }
-        std::cout << sequence[i];
-        firstElement = false;
+        std::cout << " ";
       }
     }
     std::cout << std::endl;
-  }
-  try
-  {
-    for (size_t i = 0; i < max_length; ++i)
-    {
-      unsigned long long sum = 0;
-      for (auto it = numbers.begin(); it != numbers.end(); ++it)
-      {
-        if (i < (*it).second.size())
-         {
-           if (sum > std::numeric_limits<unsigned long long>::max() - (*it).second[i])
-           {
-             throw std::overflow_error("Overflow");
-           }
-           sum += (*it).second[i];
-         }
-      }
-      sums.push_back(sum);
-    }
-  }
-  catch (const std::overflow_error& e)
-  {
-    std::cerr << e.what() << '\n';
-    return 1;
-  }
 
-  for (auto it = sums.begin(); it != sums.end(); ++it)
-  {
-    std::cout << (*it);
-    auto nextIt = it;
-    ++nextIt;
-    if (nextIt != sums.end())
+    size_t max_size = 0;
+    for (const auto& pair : numbers)
     {
-      std::cout << " ";
+      const auto& sequence = pair.second;
+      max_size = std::max(max_size, sequence.size());
     }
+
+    for (size_t i = 0; i < max_size; ++i)
+    {
+      bool firstElement = true;
+      for (const auto& pair : numbers)
+      {
+        const auto& sequence = pair.second;
+        if (i < sequence.size())
+        {
+          if (!firstElement)
+          {
+            std::cout << " ";
+          }
+          std::cout << sequence[i];
+          firstElement = false;
+        }
+      }
+      std::cout << std::endl;
+    }
+
+    try
+    {
+      for (size_t i = 0; i < max_length; ++i)
+      {
+        unsigned long long sum = 0;
+        for (auto it = numbers.begin(); it != numbers.end(); ++it)
+        {
+          if (i < (*it).second.size())
+          {
+            if (sum > std::numeric_limits<unsigned long long>::max() - (*it).second[i])
+            {
+              throw std::overflow_error("Overflow");
+            }
+            sum += (*it).second[i];
+          }
+        }
+        sums.push_back(sum);
+      }
+    }
+    catch (const std::overflow_error& e)
+    {
+      std::cerr << e.what() << '\n';
+      return 1;
+    }
+    for (auto it = sums.begin(); it != sums.end(); ++it)
+    {
+      std::cout << (*it);
+      auto nextIt = it;
+      ++nextIt;
+      if (nextIt != sums.end())
+      {
+        std::cout << " ";
+      }
+    }
+    std::cout << "\n";
   }
-  std::cout << "\n";
   if (err)
     return 1;
   return 0;
