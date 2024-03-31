b1c7eae0fd082559c6e825de1cd5106798e891a2
allleng05 <allleng04@mail.ru>
2024-03-31 18:51:51 +0300

    fix

--- gorbunova.alena/S1/main.cpp
+++ gorbunova.alena/S1/main.cpp
@@ -99,7 +99,7 @@ int main()
     }
     catch (const std::overflow_error &e)
     {
-      std::cerr << e.what() << "\n";
+      std::cerr << e.what() << '\n';
       return 1;
     }
     for (auto it = sums.begin(); it != sums.end(); ++it)
