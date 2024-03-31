fa306d3c7348ece18ac6d0af75af44d6a06d022a
ilyapanov123 <89517673116g@gmail.com>
2024-03-31 15:11:31 +0300

    sada

--- panov.ilya/S1/main.cpp
+++ panov.ilya/S1/main.cpp
@@ -12,6 +12,7 @@ bool hasOverflow(unsigned long long num) {
 
 int main() {
   Panov::List<std::pair<std::string, std::vector<unsigned long long>>> sequences;
+  Panov::List<unsigned long long> sums;
 
   std::string line;
   bool hasData = false;
@@ -35,8 +36,7 @@ int main() {
     }
 
     if (hasOverflowFlag) {
-      std::cout << temp << std::endl;
-      std::cerr << "Formed lists with exit code 1 and error message in standard error because of overflow in sequence" << std::endl;
+      std::cerr << "Formed lists with exit code 1 and error message in standard error because of overflow" << std::endl;
       return 1;
     }
 
@@ -89,6 +89,30 @@ int main() {
       }
       std::cout << std::endl;
     }
+
+    for (size_t i = 0; i < max_length; ++i) {
+      unsigned long long sum = 0;
+      for (auto it = sequences.begin(); it != sequences.end(); ++it) {
+        if (i < it->second.size()) {
+          if (sum > std::numeric_limits<unsigned long long>::max() - it->second[i]) {
+            std::cerr << "Formed lists with exit code 1 and error message in standard error because of overflow" << std::endl;
+            return 1;
+          }
+          sum += it->second[i];
+        }
+      }
+      sums.push_back(sum);
+    }
+
+    for (auto it = sums.begin(); it != sums.end(); ++it) {
+      std::cout << (*it);
+      auto nextIt = it;
+      ++nextIt;
+      if (nextIt != sums.end()) {
+        std::cout << " ";
+      }
+    }
+    std::cout << "\n";
   }
 
   return 0;
