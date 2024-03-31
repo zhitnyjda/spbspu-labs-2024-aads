28d9d62b2502e42b10339bd7df65c3f61829bbdd
ilyapanov123 <89517673116g@gmail.com>
2024-03-31 14:31:42 +0300

    sds

--- panov.ilya/S1/main.cpp
+++ panov.ilya/S1/main.cpp
@@ -105,7 +105,7 @@ int main() {
       }
     }
     catch (const std::overflow_error& e) {
-      std::cerr << e.what() << '\n';
+      std::cerr << temp << "\nFormed lists with exit code 1 and error message in standard error because of overflow in sequence: " << e.what() << '\n';
       return 1;
     }
     for (auto it = sums.begin(); it != sums.end(); ++it) {
