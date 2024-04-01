eb0f787fb16ee94560f0b7983e309213addb72ed
unknown <iulia.lis1tsyna@yandex.ru>
2024-04-01 21:38:51 +0300

    fixxx

--- lisitsyna.yulia/S1/list.hpp
+++ lisitsyna.yulia/S1/list.hpp
@@ -5,132 +5,130 @@
 #include <iterator>
 #include <stdexcept>
 
-namespace lisitsyna
+template<typename T>
+class List
 {
-  template<typename T>
-  class List
-  {
-    private:
-      struct Node
+  private:
+    struct Node
+    {
+      T value;
+      Node* next;
+      Node(T val, Node* next = nullptr) : value(val), next(next) {}
+    };
+    Node* head;
+    Node* tail;
+    size_t size;
+  public:
+    List() : head(nullptr), tail(nullptr), size(0) {}
+    List(const List& other) : head(nullptr), tail(nullptr), size(0)
+    {
+      List<T>::Node* current = other.head;
+      while (current != nullptr)
       {
-        T value;
-        Node* next;
-        Node(T val, Node* next = nullptr) : value(val), next(next) {}
-      };
-      Node* head;
-      Node* tail;
-      size_t size;
-    public:
-      List() : head(nullptr), tail(nullptr), size(0) {}
-      List(const List& other) : head(nullptr), tail(nullptr), size(0)
-      {
-        List<T>::Node* current = other.head;
-        while (current != nullptr)
-        {
-          push_back(current->value);
-          current = current->next;
-        }
+        push_back(current->value);
+        current = current->next;
       }
-      List(List&& other) noexcept : head(other.head),tail(other.tail), size(other.size)
+    }
+    List(List&& other) noexcept : head(other.head),tail(other.tail), size(other.size)
+    {
+      other.head = other.tail = nullptr;
+      other.size = 0;
+    }
+    List& operator=(const List& other)
+    {
+      if (this != &other)
       {
+        List tmp(other);
+        swap(tmp);
+      }
+      return *this;
+    }
+    List& operator=(List&& other)
+    {
+      if (this != &other)
+      {
+        clear();
+        head = other.head;
+        tail = other.tail;
+        size = other.size;
         other.head = other.tail = nullptr;
         other.size = 0;
       }
-      List& operator=(const List& other)
+      return *this;
+    }
+    ~List() { clear(); }
+    T& front()
+    {
+      if (empty())
       {
-        if (this != &other)
-        {
-          List tmp(other);
-          swap(tmp);
-        }
+        throw std::logic_error("Empty");
+      }
+      return head->value;
+    }
+    T& back()
+    {
+      if (empty())
+      {
+        throw std::logic_error("Empty");
+      }
+      return tail->value;
+    }
+    size_t get_size() const
+    {
+      return size;
+    }
+    void push_back(T val)
+    {
+      List<T>::Node* newNode = new List<T>::Node(val);
+      if (tail)
+      {
+        tail->next = newNode;
+      }
+      else
+      {
+        head = newNode;
+      }
+      tail = newNode;
+      ++size;
+    }
+    bool empty() const { return size == 0; }
+    void clear()
+    {
+      while (head)
+      {
+        List<T>::Node* temp = head;
+        head = head->next;
+        delete temp;
+      }
+      head = tail = nullptr;
+      size = 0;
+    }
+    class iterator
+    {
+      List<T>::Node* ptr;
+    public:
+      using difference_type = std::ptrdiff_t;
+      using value_type = T;
+      using pointer = T*;
+      using reference = T&;
+      using iterator_category = std::forward_iterator_tag;
+      iterator(List<T>::Node* p = nullptr) : ptr(p) {}
+      iterator& operator++()
+      {
+        if (ptr == nullptr) throw std::out_of_range("Iterator cannot be incremented past the end");
+        ptr = ptr->next;
         return *this;
       }
-      List& operator=(List&& other)
+      bool operator!=(const iterator& other) const
       {
-        if (this != &other)
-        {
-          clear();
-          head = other.head;
-          tail = other.tail;
-          size = other.size;
-          other.head = other.tail = nullptr;
-          other.size = 0;
-        }
-        return *this;
+        return ptr != other.ptr;
       }
-      ~List() { clear(); }
-      T& front()
+      reference operator*()
       {
-        if (empty())
-        {
-          throw std::logic_error("Empty");
-        }
-        return head->value;
+        if (ptr == nullptr) throw std::out_of_range("Iterator cannot be dereferenced");
+        return ptr->value;
       }
-      T& back()
-      {
-        if (empty())
-        {
-          throw std::logic_error("Empty");
-        }
-        return tail->value;
-      }
-      size_t get_size() const
-      {
-        return size;
-      }
-      void push_back(T val)
-      {
-        List<T>::Node* newNode = new List<T>::Node(val);
-        if (tail)
-        {
-          tail->next = newNode;
-        }
-        else
-        {
-          head = newNode;
-        }
-        tail = newNode;
-        ++size;
-      }
-      bool empty() const { return size == 0; }
-      void clear()
-      {
-        while (head)
-        {
-          List<T>::Node* temp = head;
-          head = head->next;
-          delete temp;
-        }
-        head = tail = nullptr;
-        size = 0;
-      }
-      class iterator
-      {
-        List<T>::Node* ptr;
-      public:
-        using difference_type = std::ptrdiff_t;
-        using value_type = T;
-        using pointer = T*;
-        using reference = T&;
-        using iterator_category = std::forward_iterator_tag;
-        iterator(List<T>::Node* p = nullptr) : ptr(p) {}
-        iterator& operator++()
-        {
-          if (ptr == nullptr) throw std::out_of_range("Iterator cannot be incremented past the end");
-          ptr = ptr->next;
-          return *this;
-        }
-        bool operator!=(const iterator& other) const
-        {
-          return ptr != other.ptr;
-        }
-        reference operator*()
-        {
-          if (ptr == nullptr) throw std::out_of_range("Iterator cannot be dereferenced");
-          return ptr->value;
-        }
-      };
+    };
     iterator begin()
     {
       return iterator(head);
