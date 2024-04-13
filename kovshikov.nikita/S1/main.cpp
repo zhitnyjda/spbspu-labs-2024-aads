3c42a5fecde81c9f862e78f5561bdcb74bad3bd2
harduserNIKITA <cumpotttt@yandex.ru>
2024-04-13 18:11:57 +0300

    scope limitation

--- kovshikov.nikita/S1/main.cpp
+++ kovshikov.nikita/S1/main.cpp
@@ -1,14 +1,13 @@
 #include "doublelist.hpp"
 #include "functions.hpp"
 
-using namespace kovshikov;
 int main()
 {
-  DoubleList<std::pair<std::string, DoubleList<size_t>>> allPairs;
+  kovshikov::DoubleList<std::pair<std::string, kovshikov::DoubleList<size_t>>> allPairs;
   bool isBigNumber = false;
-  inputPairs(allPairs, isBigNumber);
-  outputListName(allPairs);
-  outputResult(allPairs, isBigNumber);
+  kovshikov::inputPairs(allPairs, isBigNumber);
+  kovshikov::outputListName(allPairs);
+  kovshikov::outputResult(allPairs, isBigNumber);
   if(isBigNumber == true)
   {
     std::cerr << "too many numbers were entered\n";
