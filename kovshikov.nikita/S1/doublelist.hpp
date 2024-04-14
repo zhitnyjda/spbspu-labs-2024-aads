02786cc64b8c29d051b666e55a59cc124313bb29
harduserNIKITA <cumpotttt@yandex.ru>
2024-04-14 21:41:25 +0300

    fixed iterator design

--- kovshikov.nikita/S1/doublelist.hpp
+++ kovshikov.nikita/S1/doublelist.hpp
@@ -14,33 +14,7 @@ namespace kovshikov
   class DoubleList
   {
   public:
-    struct Iterator
-    {
-      friend class DoubleList< T >;
-      using this_t = Iterator;
-
-      Iterator(): node(nullptr) {};
-      Iterator(const this_t &) = default;
-      Iterator(Node::Node< T >* ptr): node(ptr) {};
-      ~Iterator() = default;
-
-      this_t & operator=(const this_t &) = default;
-
-      this_t & operator++();
-      this_t operator++(int);
-
-      this_t & operator--();
-      this_t operator--(int);
-
-      bool operator!=(const this_t & other) const;
-      bool operator==(const this_t & other) const;
-
-      T & operator*();
-      T * operator->();
-    private:
-      Node::Node< T >* node;
-    };
-
+    class Iterator;
    /* struct ConstIterator
     {
       friend class DoubleList<T>;
@@ -103,6 +77,135 @@ namespace kovshikov
     Node::Node< T >* head_;
     Node::Node< T >* tail_;
   };
+}
+
+template< typename T >
+class kovshikov::DoubleList< T >::Iterator
+{
+public:
+  friend class DoubleList< T >;
+  using this_t = Iterator;
+
+  Iterator(): node(nullptr) {};
+  Iterator(const this_t &) = default;
+  Iterator(Node::Node< T >* ptr): node(ptr) {};
+  ~Iterator() = default;
+
+  this_t & operator=(const this_t &) = default;
+
+  this_t & operator++();
+  this_t operator++(int);
+
+  this_t & operator--();
+  this_t operator--(int);
+
+  bool operator!=(const this_t & other) const;
+  bool operator==(const this_t & other) const;
+
+  T & operator*();
+  T * operator->();
+private:
+  Node::Node< T >* node;
+};
+
+template< typename T >
+typename kovshikov::DoubleList< T >::Iterator kovshikov::DoubleList< T >::begin() const
+{
+  return Iterator(head_);
+}
+
+template< typename T >
+typename kovshikov::DoubleList< T >::Iterator kovshikov::DoubleList< T >::end() const
+{
+  if(this->empty())
+  {
+    return this->begin();
+  }
+  else
+  {
+    Node::Node< T >* pastTheEnd = tail_->next;
+    return Iterator(pastTheEnd);
+  }
+}
+
+template< typename T >
+const typename kovshikov::DoubleList< T >::Iterator kovshikov::DoubleList< T >::cbegin() const
+{
+  return Iterator(head_);
+}
+
+template< typename T >
+const typename kovshikov::DoubleList< T >::Iterator kovshikov::DoubleList< T >::cend() const
+{
+  if(this->empty)
+  {
+    return this->cbegin();
+  }
+  else
+  {
+    Node::Node< T >* pastTheEnd = tail_->next;
+    return Iterator(pastTheEnd);
+  }
+}
+
+template< typename T >
+typename kovshikov::DoubleList< T >::Iterator::this_t & kovshikov::DoubleList< T >::Iterator::operator++()
+{
+  assert(node != nullptr);
+  node = node->next;
+  return *this;
+}
+
+template< typename T >
+typename kovshikov::DoubleList< T >::Iterator::this_t kovshikov::DoubleList< T >::Iterator::operator++(int)
+{
+  assert(node != nullptr);
+  this_t result(*this);
+  ++(*this);
+  return result;
+}
+
+template< typename T >
+typename kovshikov::DoubleList< T >::Iterator::this_t & kovshikov::DoubleList< T >::Iterator::operator--()
+{
+  assert(node != nullptr);
+  node = node->prev;
+  return *this;
+}
+
+template< typename T >
+typename kovshikov::DoubleList< T >::Iterator::this_t kovshikov::DoubleList< T >::Iterator::operator--(int)
+{
+  assert(node != nullptr);
+  this_t result(*this);
+  --(*this);
+  return result;
+}
+
+template< typename T >
+bool kovshikov::DoubleList< T >::Iterator::operator==(const this_t & other) const
+{
+  return node == other.node;
+}
+
+template< typename T >
+bool kovshikov::DoubleList< T >::Iterator::operator!=(const this_t & other) const
+{
+  return !(other == *this);
+}
+
+template< typename T >
+T & kovshikov::DoubleList< T >::Iterator::operator*()
+{
+  assert(node != nullptr);
+  return node->data;
+}
+
+template< typename T >
+T * kovshikov::DoubleList< T >::Iterator::operator->()
+{
+  assert(node != nullptr);
+  return std::addressof(node->data);
 }
 
 template< typename T >
@@ -368,105 +471,6 @@ void kovshikov::DoubleList< T >::assign(size_t size, const T &value)
   }
 }
 
-template< typename T >
-typename kovshikov::DoubleList< T >::Iterator kovshikov::DoubleList< T >::begin() const
-{
-  return Iterator(head_);
-}
-
-template< typename T >
-typename kovshikov::DoubleList< T >::Iterator kovshikov::DoubleList< T >::end() const
-{
-  if(this->empty())
-  {
-    return this->begin();
-  }
-  else
-  {
-    Node::Node< T >* pastTheEnd = tail_->next;
-    return Iterator(pastTheEnd);
-  }
-}
-
-template< typename T >
-const typename kovshikov::DoubleList< T >::Iterator kovshikov::DoubleList< T >::cbegin() const
-{
-  return Iterator(head_);
-}
-
-template< typename T >
-const typename kovshikov::DoubleList< T >::Iterator kovshikov::DoubleList< T >::cend() const
-{
-  if(this->empty)
-  {
-    return this->cbegin();
-  }
-  else
-  {
-    Node::Node< T >* pastTheEnd = tail_->next;
-    return Iterator(pastTheEnd);
-  }
-}
-
-template< typename T >
-typename kovshikov::DoubleList< T >::Iterator::this_t & kovshikov::DoubleList< T >::Iterator::operator++()
-{
-  assert(node != nullptr);
-  node = node->next;
-  return *this;
-}
-
-template< typename T >
-typename kovshikov::DoubleList< T >::Iterator::this_t kovshikov::DoubleList< T >::Iterator::operator++(int)
-{
-  assert(node != nullptr);
-  this_t result(*this);
-  ++(*this);
-  return result;
-}
-
-template< typename T >
-typename kovshikov::DoubleList< T >::Iterator::this_t & kovshikov::DoubleList< T >::Iterator::operator--()
-{
-  assert(node != nullptr);
-  node = node->prev;
-  return *this;
-}
-
-template< typename T >
-typename kovshikov::DoubleList< T >::Iterator::this_t kovshikov::DoubleList< T >::Iterator::operator--(int)
-{
-  assert(node != nullptr);
-  this_t result(*this);
-  --(*this);
-  return result;
-}
-
-template< typename T >
-bool kovshikov::DoubleList< T >::Iterator::operator==(const this_t & other) const
-{
-  return node == other.node;
-}
-
-template< typename T >
-bool kovshikov::DoubleList< T >::Iterator::operator!=(const this_t & other) const
-{
-  return !(other == *this);
-}
-
-template< typename T >
-T & kovshikov::DoubleList< T >::Iterator::operator*()
-{
-  assert(node != nullptr);
-  return node->data;
-}
-
-template< typename T >
-T * kovshikov::DoubleList< T >::Iterator::operator->()
-{
-  assert(node != nullptr);
-  return std::addressof(node->data);
-}
 
 /*template<typename T>
 typename kovshikov::DoubleList<T>::ConstIterator::this_t & kovshikov::DoubleList<T>::ConstIterator::operator++()
