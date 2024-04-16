522dd12abf9b6d047ec42c30e9465c14874a9773
Mark Zheleznyakov <mrqiz@internet.ru>
2024-04-16 16:29:56 +0300

    feat(S1): const iterator

---
+++ zheleznyakov.mark/S1/const-list-iterator.hpp
@@ -0,0 +1,106 @@
+#ifndef CONST_LIST_ITERATOR_HPP
+#define CONST_LIST_ITERATOR_HPP
+#include <memory>
+#include "list-item.hpp"
+
+namespace zheleznyakov
+{
+  template <typename T>
+  class ConstListIterator
+  {
+  public:
+    ConstListIterator(const ListItem<T> *listItem = nullptr);
+    ~ConstListIterator() = default;
+
+    ConstListIterator(const ConstListIterator<T> &) = default;
+    ConstListIterator<T> &operator=(const ConstListIterator<T> &) const;
+
+    ConstListIterator<T> &operator++();
+    ConstListIterator<T> &operator++(int);
+
+    ConstListIterator<T> &operator--();
+    ConstListIterator<T> &operator--(int);
+
+    T &operator*() const;
+    T *operator->() const;
+
+    bool operator!=(const ConstListIterator<T> &rhs) const;
+    bool operator==(const ConstListIterator<T> &rhs) const;
+
+    ConstListIterator begin() const;
+    ConstListIterator end() const;
+
+  private:
+    ListItem<T> *listItem;
+  };
+}
+
+template <typename T>
+zheleznyakov::ConstListIterator<T>::ConstListIterator(const ListItem<T> *listItem) : listItem(listItem) {}
+
+template <typename T>
+zheleznyakov::ConstListIterator<T> &zheleznyakov::ConstListIterator<T>::operator++()
+{
+  listItem = listItem->next;
+  return (*this);
+}
+
+template <typename T>
+zheleznyakov::ConstListIterator<T> &zheleznyakov::ConstListIterator<T>::operator++(int)
+{
+  ConstListIterator<T> result(*this);
+  ++(*this);
+  return result;
+}
+
+template <typename T>
+zheleznyakov::ConstListIterator<T> &zheleznyakov::ConstListIterator<T>::operator--()
+{
+  listItem = listItem->prev;
+  return (*this);
+}
+
+template <typename T>
+zheleznyakov::ConstListIterator<T> &zheleznyakov::ConstListIterator<T>::operator--(int)
+{
+  ConstListIterator<T> result(*this);
+  --(*this);
+  return result;
+}
+
+template <typename T>
+bool zheleznyakov::ConstListIterator<T>::operator==(const ConstListIterator<T> &rhs) const
+{
+  return listItem == rhs.listItem;
+}
+
+template <typename T>
+bool zheleznyakov::ConstListIterator<T>::operator!=(const ConstListIterator<T> &rhs) const
+{
+  return !(rhs == *this);
+}
+
+template <typename T>
+T &zheleznyakov::ConstListIterator<T>::operator*() const
+{
+  return listItem->data;
+}
+
+template <typename T>
+T *zheleznyakov::ConstListIterator<T>::operator->() const
+{
+  return std::addressof(listItem->data);
+}
+
+template<typename T>
+zheleznyakov::ConstListIterator<T> zheleznyakov::ConstListIterator<T>::begin() const
+{
+  return this;
+}
+
+template<typename T>
+zheleznyakov::ConstListIterator<T> zheleznyakov::ConstListIterator<T>::end() const
+{
+  return nullptr;
+}
+#endif
