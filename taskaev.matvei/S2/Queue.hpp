a82b4b93432efd40d8fb211f9127a7e2b4926594
amotvei <motya2228@mail.ru>
2024-04-18 21:19:07 +0300

    add my queue

---
+++ taskaev.matvei/S2/Queue.hpp
@@ -0,0 +1,46 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+#include "List.h"
+
+namespace taskaev
+{
+  template < typename T >
+  class Queue
+  {
+  public:
+    Queue() = default;
+    ~Queue() = default;
+    void push(const T& value);
+    void pop();
+    bool isEmpty() const noexcept;
+    T& front();
+  private:
+    List< T > greenfield;
+  };
+}
+
+template < typename T >
+void taskaev::Queue< T >::push(const T& value)
+{
+  greenfield.pushBack(value);
+}
+
+template < typename T >
+void taskaev::Queue< T >::pop()
+{
+  greenfield.popFront();
+}
+
+template < typename T >
+bool taskaev::Queue< T >::isEmpty() const noexcept
+{
+  return greenfield.isEmpty();
+}
+
+template < typename T >
+T& taskaev::Queue< T >::front()
+{
+  return storage.front();
+}
+
+#endif
