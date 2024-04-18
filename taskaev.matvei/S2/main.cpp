5e02f6b9b6fec7358fde07bf8ddec2fa1b3ef2f8
amotvei <motya2228@mail.ru>
2024-04-18 21:05:56 +0300

    yes, okay, well done

--- taskaev.matvei/S2/main.cpp
+++ taskaev.matvei/S2/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
+  std::string mathValue;
+  if(argc == 1)
+  {
+    while(!std::cin.eof())
+    {
+      std::getline(std::cin, mathValue);
+      try
+      {
+        //function 
+      }
+      catch(...)
+      {
+        std::cerr << e.what() << "\n";
+        return 1;
+      }
+    }
+  }
+  else if(argc == 2)
+  {
+    // не помню наверно стоит посмотреть как работали в P4 
+  }
+  else
+  {
+    std::cerr << "Error arguments, don't be greedy add more!\n";
+    return 1;
+  }
   return 0;
 }
