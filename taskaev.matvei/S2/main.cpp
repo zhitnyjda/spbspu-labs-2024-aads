0d8648ee626d93db5abc4c54b044dd22ff3ec363
amotvei <motya2228@mail.ru>
2024-04-20 02:01:11 +0300

    fixed build

--- taskaev.matvei/S2/main.cpp
+++ taskaev.matvei/S2/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char * argv[])
         convertToPostfix(queue, postfix, stack);
         results.push(calculate(postfix));
       }
-      catch(...)
+      catch(const std::exception& e)
       {
         std::cerr << e.what() << "\n";
         return 1;
