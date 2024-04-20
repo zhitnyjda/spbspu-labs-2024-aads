5b072acac7d7a41ab57b04287ca6269cc786e734
oksidmargantsa <sukachva.maria@gmail.com>
2024-04-20 20:57:38 +0300

    work on main

--- sukacheva.maria/S2/main.cpp
+++ sukacheva.maria/S2/main.cpp
@@ -9,11 +9,11 @@
 int main(int argc, char* argv[])
 {
   using namespace sukacheva;
+  Stack< long long > resultsOfCalculate;
   if (argc == 1)
   {
     try
     {
-      Stack< long long > resultsOfCalculate;
       while (!std::cin.eof())
       {
         Queue< std::string > inf = inputStatement(std::cin);
@@ -23,16 +23,6 @@ int main(int argc, char* argv[])
           resultsOfCalculate.push(calculate(test));
         }
       }
-      while (!resultsOfCalculate.empty())
-      {
-        std::cout << resultsOfCalculate.top();
-        resultsOfCalculate.pop();
-        if (!resultsOfCalculate.empty())
-        {
-          std::cout << " ";
-        }
-      }
-      std::cout << "\n";
     }
     catch (const std::exception& e)
     {
@@ -42,11 +32,46 @@ int main(int argc, char* argv[])
   }
   else if (argc == 2)
   {
-    std::ifstream input(argv[1]);
+    try
+    {
+      std::ifstream input(argv[1]);
+      if (!input)
+      {
+        std::cerr << "unable to open file !!" << "\n";
+        return 1;
+      }
+      else
+      {
+        while (!input.eof())
+        {
+          Queue< std::string > inf = inputStatement(input);
+          Queue< std::string > test = makePostfix(inf);
+          if (!test.empty())
+          {
+            resultsOfCalculate.push(calculate(test));
+          }
+        }
+      }
+    }
+    catch (const std::exception& e)
+    {
+      std::cerr << e.what() << "\n";
+      return 1;
+    }
   }
   else
   {
     std::cerr << "wrong number of args";
   }
+  while (!resultsOfCalculate.empty())
+  {
+    std::cout << resultsOfCalculate.top();
+    resultsOfCalculate.pop();
+    if (!resultsOfCalculate.empty())
+    {
+      std::cout << " ";
+    }
+  }
+  std::cout << "\n";
   return 0;
 }
