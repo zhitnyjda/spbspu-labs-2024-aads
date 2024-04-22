3a671ecb3067b77166dc10b6222617c364b522b4
0412erik <litsinger00@bk.ru>
2024-04-22 18:10:00 +0300

    update

--- litsinger.ernest/S1/list.hpp
+++ litsinger.ernest/S1/list.hpp
@@ -1,95 +1,30 @@
 #ifndef LIST_HPP
 #define LIST_HPP
-#include <iostream>
-#include "Iterator.hpp"
-#include "node.hpp"
+#include <list>
 
-namespace litsinger
-{
-  template< typename T >
-  class List
-  {
-  public:
-    List();
-    void pushBack(T value);
-    void pushFront(T value);
-    size_t getSize();
-    void popFront();
-    void popBack();
-  private:
-    head(nullptr);
-    tail(nullptr);
-    size_t size = 0;
-  };
-  template<typename T>
-  inline void List<T>::pushBack(T value)
-  {
-    Node< T >* ptr = new Node< T >(data);
-    size++;
-    ptr->prev = tail;
-    if (tail != nullptr)
-    {
-      tail->prev = ptr;
-    }
-    if (head == nullptr)
-    {
-      head = ptr;
-    }
-    tail = ptr;
-  }
-  template<typename T>
-  inline void List<T>::pushFront(T value)
-  {
-    Node< T >* ptr = new Node< T >(data);
-    size++;
-    head = ptr->next;
-    if (head != nullptr)
-    {
-      head->next = ptr;
-    }
-    if (head == nullptr)
-    {
-      head = ptr;
-    }
-    tail = ptr;
-  }
-  template<typename T>
-  inline size_t List<T>::getSize()
-  {
-      return size_t();
-  }
-  template<typename T>
-  inline void List<T>::popFront()
-  {
-    if (head == nullptr)
-    {
-      return;
-    }
-    Node<T>* ptr = head->next;
-    if (ptr != nullptr)
-    {
-      ptr->prev = nullptr;
-    }
-    delete head;
-    head = ptr;
-    size--;
-  }
-  template<typename T>
-  inline void List<T>::popBack()
-  {
-    if (tail == nullptr)
-    {
-      return;
-    }
-    Node<T> * ptr = tail->prev;
-    if (ptr != nullptr)
-    {
-      ptr->next = nullptr;
-    }
-    delete tail;
-    tail = ptr;
-    size--;
-  }
-}
+template<typename T>
+class List {
+public:
+  std::list<T> data;
 
+  void push_back(const T& value) {
+    data.push_back(value);
+  }
+
+  typename std::list<T>::iterator begin() {
+    return data.begin();
+  }
+
+  typename std::list<T>::iterator end() {
+    return data.end();
+  }
+
+  bool empty() const {
+    return data.empty();
+  }
+
+  size_t size() const {
+    return data.size();
+  }
+};
 #endif
--- litsinger.ernest/S1/main.cpp
+++ litsinger.ernest/S1/main.cpp
@@ -1,25 +1,74 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <sstream>
 #include "list.hpp"
-#include "iterator.hpp"
-#include "node.hpp"
 
-int main()
-{
-  //litsinger::List< int > listik;
-  //std::string str = "";
-  //char temp = 0;
-  //std::cin >> std::noskipws;
-  //while (!std::cin.eof())
-  //{
-	//std::cin >> temp;
-	//if (temp == '\n')
-	//{
-	  //std::cout << str << "\n";
-	  //str = "";
-	//}
-    //str += temp;
-  //}
-  //std::cin >> std::skipws;
+int main() {
+  std::string line;
+  std::list<std::pair<std::string, List<int>>> sequences;
+
+  while (std::getline(std::cin, line)) {
+    if (line.empty()) continue;
+    std::istringstream iss(line);
+    std::string name;
+    int num;
+    if (!(iss >> name)) {
+      std::cerr << "Error: Invalid input format" << std::endl;
+      return 1;
+    }
+    List<int> numbers;
+    while (iss >> num) {
+     numbers.push_back(num);
+    }
+    sequences.emplace_back(name, numbers);
+  }
+
+  if (sequences.empty()) {
+    std::cout << "0" << std::endl;
+    return 0;
+  }
+
+  for (const auto& seq : sequences) {
+    std::cout << seq.first << " ";
+  }
+  std::cout << std::endl;
+
+  bool itemsRemaining;
+  int col = 0;
+  std::vector<int> sums;
+  do {
+    itemsRemaining = false;
+    List<int> columnValues;
+    for (auto& seq : sequences) {
+      if (seq.second.size() > col) {
+        auto it = std::next(seq.second.begin(), col);
+        if (it != seq.second.end()) {
+          columnValues.push_back(*it);
+          itemsRemaining = true;
+        }
+      }
+    }
+    if (!columnValues.empty()) {
+      int sum = 0;
+      for (int val : columnValues) {
+        std::cout << val << " ";
+        sum += val;
+      }
+      sums.push_back(sum);
+      std::cout << std::endl;
+    }
+    col++;
+  } while (itemsRemaining);
+
+  if (!sums.empty()) {
+    for (int sum : sums) {
+      std::cout << sum << " ";
+    }
+    std::cout << std::endl;
+  }
+
   return 0;
 }
