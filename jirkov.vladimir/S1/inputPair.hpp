eadcc2075ec0fde7fb4a994d7d0fe33e954e8557
jirkov17 <jirkoff17@gmail.com>
2024-04-24 03:14:45 +0300

    добавлена функция для чтепния пары с ввода

---
+++ jirkov.vladimir/S1/inputPair.hpp
@@ -0,0 +1,12 @@
+#ifndef INPUT_PAIR_HPP
+#define INPUT_PAIR_HPP
+#include "list.hpp"
+#include <string>
+#include <iostream>
+
+namespace jirkov
+{
+  List<std::pair<std::string, List<size_t>>> listInput(std::istream& input);
+}
+
+#endif
