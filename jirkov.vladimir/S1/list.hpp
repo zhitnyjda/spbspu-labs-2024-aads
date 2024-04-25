7dab5c9ca5286d4795368e30dc414e95084ac9c2
jirkov17 <jirkoff17@gmail.com>
2024-04-26 00:23:28 +0300

    может пора спать?

--- jirkov.vladimir/S1/list.hpp
+++ jirkov.vladimir/S1/list.hpp
@@ -1,83 +1,211 @@
-#include "functions.hpp"
-#include "findSize.hpp"
-#include <limits>
+#ifndef LIST_HPP
+#define LIST_HPP
+#include <cstddef>
+#include <iostream>
+#include "iterator.hpp"
+#include "node.hpp"
 
-void jirkov::printListNames(const jirkov::List<std::pair<std::string, jirkov::List<size_t>>>& list)
+namespace jirkov
 {
-    auto iterator = list.begin();
-    while (iterator.node)
-    {
-        if (iterator.node != list.head_)
-        {
-            std::cout << " ";
-        }
-        std::cout << iterator.node->data.first;
-        iterator++;
-    }
-    std::cout << "\n";
+  template < typename T >
+  class List
+  {
+  public:
+    Node<T>* head_;
+    Node<T>* tail_;
+    List();
+    List(size_t n, const T& value);
+    List(const List<T>& otherList);
+    List(List<T>&& otherList);
+    ~List();
+    void pushFront(const T& data);
+    void pushBack(const T& data);
+    void popFront();
+    bool empty();
+    void clear();
+    void swap(List<T>& otherList);
+    size_t getSize();
+    Node<T>* operator[](const int index);
+    Iterator<T> begin() const;
+    Iterator<T> end() const;
+    const Iterator<T> cbegin() const;
+    const Iterator<T> cend() const;
+  };
 }
-void jirkov::printValues(const jirkov::List<std::pair<std::string,jirkov::List<size_t>>>&list,size_t maxSize,jirkov::List<size_t>&sums)
-{
-    size_t sum = 0;
-    bool overflowError = false;
 
-    for (size_t index = 0; index < maxSize; index++)
-    {
-        auto iterator = list.begin();
-        while (iterator.node)
-        {
-            auto& secondList = iterator.node->data.second;
-            if (index < secondList.getSize())
-            {
-                size_t value = secondList[index]->data;
-                if (sum > 0)
-                {
-                    std::cout << " ";
-                }
-                std::cout << value;
-                checkForOverflow(sum, value);
-                sum += value;
-            }
-            iterator++;
-        }
-        std::cout << "\n";
+template< typename T >
+jirkov::List< T >::List() :
+  head_(nullptr),
+  tail_(nullptr)
+{}
 
-        if (!overflowError)
-        {
-            sums.pushBack(sum);
-        }
-        sum = 0;
-        overflowError = false;
-    }
-}
-void jirkov::printSums(jirkov::List<size_t>& sums)
+template< typename T >
+jirkov::List< T >::List(size_t n, const T& value)
 {
-    if (sums.empty())
-    {
-        std::cout << "0\n";
-    }
-    else
-    {
-        auto it = sums.begin();
-        while (it.node)
-        {
-            if (it.node != sums.head_)
-            {
-                std::cout << " ";
-            }
-            std::cout << it.node->data;
-            it++;
-        }
-        std::cout << "\n";
-    }
+  head_ = nullptr;
+  tail_ = nullptr;
+  for (size_t i = 0; i < n; i++)
+  {
+    pushBack(value);
+  }
 }
-void jirkov::checkForOverflow(size_t sum, size_t value)
-{
-    const size_t maximum = std::numeric_limits<size_t>::max();
 
-    if (maximum - sum < value)
-    {
-        std::cout << "\n";
-        throw std::overflow_error("Overflow error");
-    }
+template< typename T >
+jirkov::List< T >::List(const List< T >& otherList)
+{
+  head_ = nullptr;
+  tail_ = nullptr;
+  Node< T >* head = otherList.head_;
+  while (head)
+  {
+    pushBack(head->data);
+    head = head->next;
+  }
 }
+
+template< typename T >
+jirkov::List< T >::List(List&& otherList)
+{
+  head_ = otherList.head_;
+  tail_ = otherList.tail_;
+  otherList.head_ = nullptr;
+  otherList.tail_ = nullptr;
+}
+
+template< typename T >
+jirkov::List< T >::~List()
+{
+  clear();
+}
+
+template< typename T >
+void jirkov::List< T >::pushFront(const T& data)
+{
+  Node< T >* node = new Node< T >(data);
+  if (head_ != nullptr)
+  {
+    node->next = head_;
+    head_ = node;
+  }
+  else
+  {
+    head_ = tail_ = node;
+  }
+}
+
+template< typename T >
+void jirkov::List< T >::pushBack(const T& data)
+{
+  Node< T >* node = new Node< T >(data);
+  if (head_ == nullptr)
+  {
+    head_ = tail_ = node;
+  }
+  else
+  {
+    tail_->next = node;
+    tail_ = node;
+  }
+}
+
+template< typename T >
+size_t jirkov::List< T >::getSize()
+{
+  jirkov::Node< T >* head = head_;
+  size_t size = 0ull;
+  while (head)
+  {
+    size++;
+    head = head->next;
+  }
+  return size;
+}
+
+
+template< typename T >
+void jirkov::List< T >::popFront()
+{
+  if (head_ == tail_)
+  {
+    delete tail_;
+    head_ = tail_ = nullptr;
+  }
+  if (head_ != nullptr)
+  {
+    Node< T >* node = head_;
+    head_ = node->next;
+    delete node;
+  }
+}
+
+template< typename T >
+bool jirkov::List< T >::empty()
+{
+  return head_ == nullptr ? true : false;
+}
+template< typename T >
+void jirkov::List< T >::clear()
+{
+  while (head_)
+  {
+    popFront();
+  }
+}
+
+template< typename T >
+void jirkov::List< T >::swap(List< T >& otherList)
+{
+  jirkov::Node< T >* head = head_;
+  head_ = otherList.head_;
+  otherList.head_ = head;
+  delete head;
+  jirkov::Node< T >* tail = tail_;
+  tail_ = otherList.tail_;
+  otherList.tail_ = tail;
+  delete tail;
+}
+
+template< typename T >
+jirkov::Node< T >* jirkov::List< T >::operator[](const int index)
+{
+  if (empty() == true)
+  {
+    return nullptr;
+  }
+  Node< T >* head = head_;
+  for (int i = 0; i < index; i++)
+  {
+    head = head->next;
+    if (!head)
+    {
+      return nullptr;
+    }
+  }
+  return head;
+}
+
+template< typename T >
+jirkov::Iterator< T > jirkov::List< T >::begin() const
+{
+  return Iterator< T >(head_);
+}
+
+template< typename T >
+jirkov::Iterator< T > jirkov::List< T >::end() const
+{
+  return Iterator< T >(tail_->next);
+}
+
+template< typename T >
+const jirkov::Iterator< T > jirkov::List< T >::cbegin() const
+{
+  return Iterator< T >(head_);
+}
+
+template< typename T >
+const jirkov::Iterator< T > jirkov::List< T >::cend() const
+{
+  return Iterator< T >(tail_->next);
+}
+
+#endif
--- jirkov.vladimir/S1/main.cpp
+++ jirkov.vladimir/S1/main.cpp
@@ -6,25 +6,25 @@
 
 int main()
 {
-    using namespace jirkov;
-    try
+  using namespace jirkov;
+  try
+  {
+    List<std::pair<std::string, List<size_t>>> list = listInput(std::cin);
+    List<size_t> sums;
+    if (list.empty())
     {
-        List<std::pair<std::string, List<size_t>>> list = listInput(std::cin);
-        List<size_t> sums;
-        if (list.empty())
-        {
-            std::cout << "0\n";
-            return 0;
-        }
-        size_t maxSize = findSize(list);
-        printListNames(list);
-        printValues(list, maxSize, sums);
-        printSums(sums);
-        return 0;
+      std::cout << "0\n";
+      return 0;
     }
-    catch (std::overflow_error& e)
-    {
-        std::cerr << e.what() << "\n";
-        return 1;
-    }
+    size_t maxSize = findSize(list);
+    printListNames(list);
+    printValues(list, maxSize, sums);
+    printSums(sums);
+    return 0;
+  }
+  catch (std::overflow_error& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
 }
