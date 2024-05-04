2baa1a7c3395312bdc7821b80e0973cb8affbb3a
Vlad0sEnIgma345 <vlad0senigma345@gmail.com>
2024-05-05 01:18:40 +0300

    deleted struct.hpp

--- mihalchenko.vladislav/S1/functions.hpp
+++ mihalchenko.vladislav/S1/functions.hpp
@@ -5,13 +5,6 @@
 
 namespace mihalchenko
 {
-  struct t_ret
-  {
-    size_t summa;
-    bool flgNewStr;
-    bool resultFunc;
-  };
-
   using mainList = mihalchenko::List< std::pair< std::string, mihalchenko::List< size_t > > >;
   using integerList = mihalchenko::List< size_t >;
   size_t printName(mainList &mixedList, size_t mixedListSize, size_t maxLenOfSecondList);
