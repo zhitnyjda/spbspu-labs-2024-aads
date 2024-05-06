ed2ed32291df39d9b41ea0b39e82e37cecd5280c
AlexNik <sahan@DESKTOP-R8OTJOJ>
2024-05-06 20:22:54 +0300

    adding user input

--- nikiforov.alexander/S2/main.cpp
+++ nikiforov.alexander/S2/main.cpp
@@ -5,30 +5,40 @@ int main(int argc, char* argv[])
 {
   using namespace nikiforov;
 
-  if (argc != 2)
-  {
-    std::cerr << "Error: wrong number of parameters\n";
-    return 2;
-  }
-
-  std::ifstream input(argv[1]);
-  if (!input)
-  {
-    std::cerr << "Error: unable to open the file\n";
-    return 2;
-  }
-
   List< long long > Result;
   Queue< dataTypes > Postfix;
   std::string infix = "";
 
   try
   {
-    while (!input.eof())
+    if (argc == 2)
     {
-      std::getline(input, infix);
-      convertToPostfix(infix, Postfix);
-      calculation(Postfix, Result);
+      std::ifstream input(argv[1]);
+      if (!input)
+      {
+        std::cerr << "Error: unable to open the file\n";
+        return 2;
+      }
+
+      while (!input.eof())
+      {
+        std::getline(input, infix);
+        convertToPostfix(infix, Postfix);
+        calculation(Postfix, Result);
+      }
+    }
+    else if (argc == 1)
+    {
+      while (!std::cin.eof())
+      {
+        std::getline(std::cin, infix);
+        convertToPostfix(infix, Postfix);
+        calculation(Postfix, Result);
+      }
+    }
+    else
+    {
+      std::cerr << "Error: wrong number of parameters\n";
     }
 
     for (List< long long >::ConstIterator iter = Result.cbegin(); iter != Result.cend(); ++iter)
