35b8e6ce71452f56bb4d3722b9002b14daa4dfe5
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-05-06 15:24:44 +0300

    Update SequenceHandler.cpp

--- zhitnyj.dmitriy/S1/SequenceHandler.cpp
+++ zhitnyj.dmitriy/S1/SequenceHandler.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <limits>
-#include "SequenceHandler.hpp"
+#include "SequenceHandler.h"
 
 void SequenceHandler::addSequence(const std::string &name, const List< unsigned long long > &sequence) {
   names.push_back(name);
   sequences.push_back(sequence);
 }
 
-void SequenceHandler::printSequences() {
+void SequenceHandler::printSequencesNames() {
   for (auto it = names.begin(); it != names.end(); ++it) {
     std::cout << *it << (it + 1 == names.end() ? "" : " ");
   }
@@ -16,7 +16,7 @@ void SequenceHandler::printSequences() {
   }
 }
 
-void SequenceHandler::rearrangeAndPrint() {
+void SequenceHandler::calculateSums() {
   size_t maxLen = 0;
   for (auto &seq: sequences) {
     maxLen = std::max(maxLen, seq.size());
@@ -24,28 +24,15 @@ void SequenceHandler::rearrangeAndPrint() {
 
   unsigned long long *sums = new unsigned long long[maxLen]{};
 
-  int s = 0;
   for (size_t i = 0; i < maxLen; ++i) {
     for (const auto &seq: sequences) {
-      auto it = seq.begin();
-
-      for (size_t j = 0; j < i && it != seq.end(); ++j) {
-        ++it;
-      }
-      if (it != seq.end()) {
-        std::cout << (s ? " " : "");
-        s = 1;
-        std::cout << *it;
-        if (sums[i] > std::numeric_limits< unsigned long long >::max() - *it) {
-          std::cout << "\n";
-          delete[] sums;
+      if (i < seq.size()) {
+        if (sums[i] > std::numeric_limits< unsigned long long >::max() - seq[i]) {
           throw std::overflow_error("Sum of numbers is too big.");
         }
-        sums[i] += *it;
+        sums[i] += seq[i];
       }
     }
-    std::cout << "\n";
-    s = 0;
   }
 
   if (!maxLen) {
@@ -57,4 +44,23 @@ void SequenceHandler::rearrangeAndPrint() {
   }
 
   delete[] sums;
+}
+
+void SequenceHandler::printSequences() {
+  size_t maxLen = 0;
+  for (auto &seq: sequences) {
+    maxLen = std::max(maxLen, seq.size());
+  }
+
+  size_t count = 0;
+  for (size_t i = 0; i < maxLen; ++i) {
+    for (const auto &seq: sequences) {
+      if (i < seq.size()) {
+        std::cout << (count ? " " : "") << seq[i];
+      }
+      count++;
+    }
+    std::cout << "\n";
+    count = 0;
+  }
 }
