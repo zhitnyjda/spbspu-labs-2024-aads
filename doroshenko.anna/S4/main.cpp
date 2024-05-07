aa5ac471bdc576aaaf7168d50e40e003d7fa1030
anyadg <anna_giraffe@mail.ru>
2024-05-07 14:46:17 +0300

    позорный коммит

--- doroshenko.anna/S4/main.cpp
+++ doroshenko.anna/S4/main.cpp
@@ -5,37 +5,37 @@
 
 int main()
 {
-	using namespace doroshenko;
-	BST< std::string, BST< long long, std::string > > treeOfDicts;
-	BST< long long, std::string > dict2;
-	//inputTree(std::cin, treeOfDicts);
+  using namespace doroshenko;
+  BST< std::string, BST< long long, std::string > > treeOfDicts;
+  BST< long long, std::string > dict2;
+  //inputTree(std::cin, treeOfDicts);
 
-	std::string name1 = "dict1";
-	long long key1 = 1;
-	std::string value1 = "value1";
+  std::string name1 = "dict1";
+  long long key1 = 1;
+  std::string value1 = "value1";
 
-	std::string name2 = "dict2";
-	long long key3 = 3;
-	std::string value3 = "value3";
+  std::string name2 = "dict2";
+  long long key3 = 3;
+  std::string value3 = "value3";
 
-	long long key2 = 2;
-	std::string value2 = "value2";
-	BST< long long, std::string> dict1;
-	dict1.insert(key1, value1);
-	dict1.insert(key2, value2);
+  long long key2 = 2;
+  std::string value2 = "value2";
+  BST< long long, std::string> dict1;
+  dict1.insert(key1, value1);
+  dict1.insert(key2, value2);
 
-	dict2.insert(key1, value1);
-	dict2.insert(key3, value3);
+  dict2.insert(key1, value1);
+  dict2.insert(key3, value3);
 
-	std::cout << "\n";
+  std::cout << "\n";
 
   /*for (auto it = dict.cbegin(); it != dict.cend(); ++it)
   {
 	  std::cout << "(" << it->first << ", " << it->second << ") " << "\n";
   }*/
-	treeOfDicts.insert(name1, dict1);
-	treeOfDicts.insert(name2, dict2);
+  treeOfDicts.insert(name1, dict1);
+  treeOfDicts.insert(name2, dict2);
 
-	complement(treeOfDicts);
-	print(treeOfDicts);
+  complement(treeOfDicts);
+  print(treeOfDicts);
 }
