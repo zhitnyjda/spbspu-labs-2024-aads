9009cde4dd256092c4dbb1b5a96e27d52262ec20
amotvei <motya2228@mail.ru>
2024-05-08 14:00:42 +0300

    fixed CG and ET

--- taskaev.matvei/S2/IdentifierMath.hpp
+++ taskaev.matvei/S2/IdentifierMath.hpp
@@ -33,46 +33,10 @@ namespace taskaev
       char parantheses;
     };
     IdentifierMath() = default;
-    explicit IdentifierMath(const std::string& item)
-    {
-      if (!isValue(item))
-      {
-        throw std::invalid_argument(" SOS are you Ok?");
-      }
-      if (isdigit(item[0])) {
-        types = operand;
-        operands.data = std::stoll(item);
-      }
-      else if (isOperation(item[0])) {
-        types = operation;
-        operations.data = item[0];
-      }
-      else if ('(' == item[0]) {
-        types = openParantheses;
-        parantheses = item[0];
-      }
-      else
-      {
-        types = closedParantheses;
-        parantheses = item[0];
-      }
-    }
+    explicit IdentifierMath(const std::string& item);
     private:
-      bool isValue(const std::string& item)
-      {
-        for (size_t i = 0; i < item.length(); i++)
-        {
-          if (!isdigit(item[i]) && (item.length() > 1))
-          {
-            return false;
-          }
-        }
-        return true;
-      }
-      bool isOperation(char c)
-      {
-        return (c == '+' || c == '-' || c == '*' || c == '/' || c == '%');
-      }
+      bool isValue(const std::string& item);
+      bool isOperation(char c);
   };
 }
 
