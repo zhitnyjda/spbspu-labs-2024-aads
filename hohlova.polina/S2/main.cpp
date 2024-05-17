32aa1e1557327e17b56d4de3ecfc931f483707ec
phohlova <hohlova.pv@edu.spbstu.ru>
2024-05-18 01:22:59 +0300

    fix

--- hohlova.polina/S2/main.cpp
+++ hohlova.polina/S2/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
 
   while (!results.empty())
   {
-    std::cout << results.top() << ' ';
+    std::cout << results.top();
     results.pop();
   }
   std::cout << "\n";
