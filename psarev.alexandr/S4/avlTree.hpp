d3b85a68de6716b4540d28ee42006d8ce27ff752
Александр Псарев <alexandrpsarev5555@gmail.com>
2024-05-17 20:04:11 +0300

    constructors implemented

--- psarev.alexandr/S4/avlTree.hpp
+++ psarev.alexandr/S4/avlTree.hpp
@@ -15,11 +15,18 @@ namespace psarev
     using iter = Iterator;
     using dataType = std::pair< Key, Value >;
 
-    void push(Key k, Value v);
-    Value get(Key k);
-    Value drop(Key k);
+    avlTree();
+    avlTree(const avlTree& that);
+    avlTree(size_t& initSize, dataType& initData);
+    avlTree(std::initializer_list< dataType > initlList);
+    ~avlTree();
+
+    //void push(Key k, Value v);
+    //Value get(Key k);
+    //Value drop(Key k);
 
   private:
+
     struct Unit
     {
       friend class avlTree;
@@ -38,239 +45,284 @@ namespace psarev
         this->right = right_;
         this->hgt = hgt_;
       }
+
+      Unit* treeRoot;
+      size_t treeSize;
     };
   };
+}
 
-  template < typename Key, typename Value, typename Compare >
-  class psarev::avlTree< Key, Value, Compare >::ConstIterator : public std::iterator< std::bidirectional_iterator_tag, dataType >
+template < typename Key, typename Value, typename Compare >
+class psarev::avlTree< Key, Value, Compare >::ConstIterator : public std::iterator< std::bidirectional_iterator_tag, dataType >
+{
+public:
+  friend class avlTree< Key, Value, Compare >;
+  using this_t = ConstIterator;
+
+  ConstIterator()
   {
-  public:
-    friend class avlTree< Key, Value, Compare >;
-    using this_t = ConstIterator;
-
-    ConstIterator()
-    {
-      unit(nullptr), root(nullptr)
-    }
-    ~ConstIterator() = default;
-
-    this_t& operator=(const this_t&) = default;
-
-    this_t& operator++();
-    this_t operator++(int);
-    this_t& operator--();
-    this_t operator--(int);
-
-    const dataType& operator*() const;
-    const dataType* operator->() const;
-
-    bool operator==(const this_t& rhs) const;
-    bool operator!=(const this_t& rhs) const;
-
-  private:
-    Unit* unit;
-    Unit* root;
-    ConstIterator(Unit* unit, Unit* root);
-  };
-
-  template < typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::ConstIterator& psarev::avlTree< Key, Value, Compare >::ConstIterator::operator++()
-  {
-    if (unit->right != nullptr)
-    {
-      unit = unit->right;
-      while (unit->left != nullptr)
-      {
-        unit = unit->left;
-      }
-    }
-    else
-    {
-      while ((unit == unit->ancest->right) && (unit->ancest != nullptr))
-      {
-        unit = unit->ancest;
-      }
-      unit = unit->ancest;
-    }
-    return *this;
+    unit(nullptr), root(nullptr)
   }
+  ~ConstIterator() = default;
 
-  template< typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::ConstIterator psarev::avlTree< Key, Value, Compare >::ConstIterator::operator++(int)
-  {
-    this_t inked(*this);
-    ++(*this);
-    return inked;
-  }
+  this_t& operator=(const this_t&) = default;
 
-  template < typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::ConstIterator& psarev::avlTree< Key, Value, Compare >::ConstIterator::operator--()
+  this_t& operator++();
+  this_t operator++(int);
+  this_t& operator--();
+  this_t operator--(int);
+
+  const dataType& operator*() const;
+  const dataType* operator->() const;
+
+  bool operator==(const this_t& rhs) const;
+  bool operator!=(const this_t& rhs) const;
+
+private:
+  Unit* unit;
+  Unit* root;
+  ConstIterator(Unit* unit, Unit* root);
+};
+
+template < typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::ConstIterator& psarev::avlTree< Key, Value, Compare >::ConstIterator::operator++()
+{
+  if (unit->right != nullptr)
   {
-    if (unit == nullptr)
-    {
-      unit = root;
-      while (unit->right != nullptr)
-      {
-        unit = unit->right;
-      }
-    }
-    else if (unit->left != nullptr)
+    unit = unit->right;
+    while (unit->left != nullptr)
     {
       unit = unit->left;
-      while (unit->right != nullptr)
-      {
-        unit = unit->right;
-      }
     }
-    else
+  }
+  else
+  {
+    while ((unit == unit->ancest->right) && (unit->ancest != nullptr))
     {
-      while ((unit == unit->ancest->left) && (unit->ancest != nullptr))
-      {
-        unit = unit->ancest;
-      }
       unit = unit->ancest;
     }
-    return *this;
+    unit = unit->ancest;
   }
-
-  template< typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::ConstIterator psarev::avlTree< Key, Value, Compare >::ConstIterator::operator--(int)
-  {
-    this_t deked(*this);
-    --(*this);
-    return deked;
-  }
-
-  template < typename Key, typename Value, typename Compare >
-  const typename psarev::avlTree< Key, Value, Compare >::dataType& psarev::avlTree< Key, Value, Compare >::ConstIterator::operator*() const
-  {
-    return unit->data;
-  }
-
-  template < typename Key, typename Value, typename Compare >
-  const typename psarev::avlTree< Key, Value, Compare >::dataType* psarev::avlTree< Key, Value, Compare >::ConstIterator::operator->() const
-  {
-    return &(unit->data);
-  }
-
-  template < typename Key, typename Value, typename Compare >
-  bool psarev::avlTree< Key, Value, Compare >::ConstIterator::operator==(const this_t& that) const
-  {
-    return unit == that.unit;
-  }
-
-  template < typename Key, typename Value, typename Compare >
-  bool psarev::avlTree< Key, Value, Compare >::ConstIterator::operator!=(const this_t& that) const
-  {
-    return !(that == *this);
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  class psarev::avlTree< Key, Value, Compare >::Iterator : public std::iterator< std::bidirectional_iterator_tag, dataType >
-  {
-  public:
-    friend class Tree< Key, Value, Compare >;
-    using this_t = Iterator;
-    Iterator();
-    Iterator(ConstIterator constIter);
-    Iterator(const this_t&) = default;
-    ~Iterator() = default;
-
-    this_t& operator=(const this_t&) = default;
-    this_t& operator++();
-    this_t operator++(int);
-    this_t& operator--();
-    this_t operator--(int);
-    this_t operator+(size_t index);
-
-    dataType& operator*();
-    dataType* operator->();
-    const dataType& operator*() const;
-    const dataType* operator->() const;
-
-    bool operator==(const this_t&) const;
-    bool operator!=(const this_t&) const;
-
-  private:
-    ConstIterator imIter;
-  };
-
-  template< typename Key, typename Value, typename Compare >
-  psarev::avlTree< Key, Value, Compare >::Iterator::Iterator() :
-    imIter(ConstIterator())
-  {}
-
-  template< typename Key, typename Value, typename Compare >
-  psarev::avlTree< Key, Value, Compare >::Iterator::Iterator(ConstIterator constIter) :
-    imIter(constIter)
-  {}
-
-  template< typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::Iterator& psarev::avlTree< Key, Value, Compare >::Iterator::operator++()
-  {
-    assert(imIter != ConstIterator());
-    imIter++;
-    return imIter;
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::Iterator psarev::avlTree< Key, Value, Compare >::Iterator::operator++(int)
-  {
-    ++imIter;
-    return imIter;
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::Iterator& psarev::avlTree< Key, Value, Compare >::Iterator::operator--()
-  {
-    assert(imIter != nullptr);
-    --imIter;
-    return imIter;
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::Iterator psarev::avlTree< Key, Value, Compare >::Iterator::operator--(int)
-  {
-    --imIter;
-    return imIter;
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::dataType& psarev::avlTree< Key, Value, Compare >::Iterator::operator*()
-  {
-    return imIter.unit->data;
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename psarev::avlTree< Key, Value, Compare >::dataType* psarev::avlTree< Key, Value, Compare >::Iterator::operator->()
-  {
-    return &(imIter.unit->data);
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  const typename psarev::avlTree< Key, Value, Compare >::dataType& psarev::avlTree< Key, Value, Compare >::Iterator::operator*() const
-  {
-    return imIter.node_->data;
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  const typename psarev::avlTree< Key, Value, Compare >::dataType* psarev::avlTree< Key, Value, Compare >::Iterator::operator->() const
-  {
-    return &(imIter.unit->data);
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  bool psarev::avlTree< Key, Value, Compare >::Iterator::operator==(const this_t& that) const
-  {
-    return imIter == that.imIter;
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  bool psarev::avlTree< Key, Value, Compare >::Iterator::operator!=(const this_t& that) const
-  {
-    return !(that == *this);
-  }
-
+  return *this;
 }
+
+template< typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::ConstIterator psarev::avlTree< Key, Value, Compare >::ConstIterator::operator++(int)
+{
+  this_t inked(*this);
+  ++(*this);
+  return inked;
+}
+
+template < typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::ConstIterator& psarev::avlTree< Key, Value, Compare >::ConstIterator::operator--()
+{
+  if (unit == nullptr)
+  {
+    unit = root;
+    while (unit->right != nullptr)
+    {
+      unit = unit->right;
+    }
+  }
+  else if (unit->left != nullptr)
+  {
+    unit = unit->left;
+    while (unit->right != nullptr)
+    {
+      unit = unit->right;
+    }
+  }
+  else
+  {
+    while ((unit == unit->ancest->left) && (unit->ancest != nullptr))
+    {
+      unit = unit->ancest;
+    }
+    unit = unit->ancest;
+  }
+  return *this;
+}
+
+template< typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::ConstIterator psarev::avlTree< Key, Value, Compare >::ConstIterator::operator--(int)
+{
+  this_t deked(*this);
+  --(*this);
+  return deked;
+}
+
+template < typename Key, typename Value, typename Compare >
+const typename psarev::avlTree< Key, Value, Compare >::dataType& psarev::avlTree< Key, Value, Compare >::ConstIterator::operator*() const
+{
+  return unit->data;
+}
+
+template < typename Key, typename Value, typename Compare >
+const typename psarev::avlTree< Key, Value, Compare >::dataType* psarev::avlTree< Key, Value, Compare >::ConstIterator::operator->() const
+{
+  return &(unit->data);
+}
+
+template < typename Key, typename Value, typename Compare >
+bool psarev::avlTree< Key, Value, Compare >::ConstIterator::operator==(const this_t& that) const
+{
+  return unit == that.unit;
+}
+
+template < typename Key, typename Value, typename Compare >
+bool psarev::avlTree< Key, Value, Compare >::ConstIterator::operator!=(const this_t& that) const
+{
+  return !(that == *this);
+}
+
+template< typename Key, typename Value, typename Compare >
+class psarev::avlTree< Key, Value, Compare >::Iterator : public std::iterator< std::bidirectional_iterator_tag, dataType >
+{
+public:
+  friend class Tree< Key, Value, Compare >;
+  using this_t = Iterator;
+  Iterator();
+  Iterator(ConstIterator constIter);
+  Iterator(const this_t&) = default;
+  ~Iterator() = default;
+
+  this_t& operator=(const this_t&) = default;
+  this_t& operator++();
+  this_t operator++(int);
+  this_t& operator--();
+  this_t operator--(int);
+  this_t operator+(size_t index);
+
+  dataType& operator*();
+  dataType* operator->();
+  const dataType& operator*() const;
+  const dataType* operator->() const;
+
+  bool operator==(const this_t&) const;
+  bool operator!=(const this_t&) const;
+
+private:
+  ConstIterator imIter;
+};
+
+template< typename Key, typename Value, typename Compare >
+psarev::avlTree< Key, Value, Compare >::Iterator::Iterator() :
+  imIter(ConstIterator())
+{}
+
+template< typename Key, typename Value, typename Compare >
+psarev::avlTree< Key, Value, Compare >::Iterator::Iterator(ConstIterator constIter) :
+  imIter(constIter)
+{}
+
+template< typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::Iterator& psarev::avlTree< Key, Value, Compare >::Iterator::operator++()
+{
+  assert(imIter != ConstIterator());
+  imIter++;
+  return imIter;
+}
+
+template< typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::Iterator psarev::avlTree< Key, Value, Compare >::Iterator::operator++(int)
+{
+  ++imIter;
+  return imIter;
+}
+
+template< typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::Iterator& psarev::avlTree< Key, Value, Compare >::Iterator::operator--()
+{
+  assert(imIter != nullptr);
+  --imIter;
+  return imIter;
+}
+
+template< typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::Iterator psarev::avlTree< Key, Value, Compare >::Iterator::operator--(int)
+{
+  --imIter;
+  return imIter;
+}
+
+template< typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::dataType& psarev::avlTree< Key, Value, Compare >::Iterator::operator*()
+{
+  return imIter.unit->data;
+}
+
+template< typename Key, typename Value, typename Compare >
+typename psarev::avlTree< Key, Value, Compare >::dataType* psarev::avlTree< Key, Value, Compare >::Iterator::operator->()
+{
+  return &(imIter.unit->data);
+}
+
+template< typename Key, typename Value, typename Compare >
+const typename psarev::avlTree< Key, Value, Compare >::dataType& psarev::avlTree< Key, Value, Compare >::Iterator::operator*() const
+{
+  return imIter.node_->data;
+}
+
+template< typename Key, typename Value, typename Compare >
+const typename psarev::avlTree< Key, Value, Compare >::dataType* psarev::avlTree< Key, Value, Compare >::Iterator::operator->() const
+{
+  return &(imIter.unit->data);
+}
+
+template< typename Key, typename Value, typename Compare >
+bool psarev::avlTree< Key, Value, Compare >::Iterator::operator==(const this_t& that) const
+{
+  return imIter == that.imIter;
+}
+
+template< typename Key, typename Value, typename Compare >
+bool psarev::avlTree< Key, Value, Compare >::Iterator::operator!=(const this_t& that) const
+{
+  return !(that == *this);
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+template< typename Key, typename Value, typename Compare >
+psarev::avlTree< Key, Value, Compare >::avlTree() :
+  treeRoot(nullptr), treeSize(0)
+{}
+
+template<typename Key, typename Value, typename Compare>
+psarev::avlTree< Key, Value, Compare >::avlTree(const avlTree& that) :
+  treeRoot(that.treeRoot), treeSize(that.treeSize)
+{}
+
+template<typename Key, typename Value, typename Compare>
+psarev::avlTree< Key, Value, Compare >::avlTree(size_t& initSize, dataType& initData)
+{
+  treeRoot = nullptr;
+  treeSize = 0;
+  for (size_t i = 0; i < initSize; ++i)
+  {
+    insert(iniData);
+  }
+}
+
+template < typename Key, typename Value, typename Compare >
+psarev::avlTree< Key, Value, Compare >::avlTree(std::initializer_list< dataType > initlList)
+{
+  treeRoot = nullptr;
+  treeSize = 0;
+  for (auto data : initList)
+  {
+    insert(data);
+  }
+}
+
+template< typename Key, typename Value, typename Compare >
+psarev::avlTree< Key, Value, Compare >::~avlTree()
+{
+  clear();
+  root = nullptr;
+}
+
+
 
 #endif
