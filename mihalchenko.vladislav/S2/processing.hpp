ca643a76f8ce36fff75a1166f6f15230da1e7c45
Vlad0sEnIgma345 <vlad0senigma345@gmail.com>
2024-05-20 14:41:43 +0300

    style(S2): Поправлен перенос с уровня скобки

--- mihalchenko.vladislav/S2/processing.hpp
+++ mihalchenko.vladislav/S2/processing.hpp
@@ -12,7 +12,7 @@ namespace mihalchenko
   void printedResult(std::ostream &out, Stack< long long > &calcRes, size_t &num);
   size_t calculatePostFix(Queue< std::string > &resiveDigit, Stack< long long > &calculateResult);
   bool bildStrPostFix(std::string &currentStr, size_t currentStrSize, Queue< std::string > &resiveDigit,
-    Stack< std::string > &resiveControl);
+                        Stack< std::string > &resiveControl);
 }
 
 #endif
