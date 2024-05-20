9dd6ca037416f2f9e4ae8ed2311125a64ff4dcb6
phohlova <hohlova.pv@edu.spbstu.ru>
2024-05-21 01:29:20 +0300

    fix

--- hohlova.polina/S2/main.cpp
+++ hohlova.polina/S2/main.cpp
@@ -1,29 +1,26 @@
-#include <iostream>
 #include "ExpressionCalc.hpp"
 #include <fstream>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
   hohlova::ExpressionCalc calculator;
   hohlova::Stack< long long > results;
+  if (argc == 1)
+  {
+    calculator.readExpression(std::cin);
+  }
+  else if (argc == 2)
+  {
+    std::ifstream stream(argv[1]);
+    calculator.readExpression(stream);
+  }
+  else
+  {
+    std::cerr << "Invalid number of command line arguments";
+  }
   try
   {
-    if (argc == 1)
-    {
-      calculator.readExpression(std::cin);
-    }
-    else if (argc == 2)
-    {
-      std::ifstream stream(argv[1]);
-      if (stream.eof() == 0)
-        throw std::runtime_error("File is empty!\n");
-
-      calculator.readExpression(stream);
-    }
-    else
-    {
-      std::cerr << "Invalid number of command line arguments\n";
-    }
     calculator.CalculateExpressions(results);
   }
   catch (const std::runtime_error& err)
