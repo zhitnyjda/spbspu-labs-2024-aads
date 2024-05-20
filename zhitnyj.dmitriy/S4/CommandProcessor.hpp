96dbc88bf8fff7ee5259d379decbfd7c738bc8c2
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-05-20 11:03:41 +0300

    Update CommandProcessor.hpp

--- zhitnyj.dmitriy/S4/CommandProcessor.hpp
+++ zhitnyj.dmitriy/S4/CommandProcessor.hpp
@@ -6,11 +6,11 @@
 #include <functional>
 #include "BinarySearchTree.hpp"
 
-void loadTreeFromFile(const std::string &filename, bsTree< std::string, bsTree< long long, std::string>> &dicts);
-void handlePrint(bsTree< std::string, bsTree< long long, std::string>> &dicts);
-void handleComplement(bsTree< std::string, bsTree< long long, std::string>> &dicts);
-void handleIntersect(bsTree< std::string, bsTree< long long, std::string>> &dicts);
-void handleUnion(bsTree< std::string, bsTree< long long, std::string>> &dicts);
+void loadTreeFromFile(const std::string &filename, bsTree< std::string, bsTree< long long, std::string > > &dicts);
+void handlePrint(bsTree< std::string, bsTree< long long, std::string > > &dicts);
+void handleComplement(bsTree< std::string, bsTree< long long, std::string > > &dicts);
+void handleIntersect(bsTree< std::string, bsTree< long long, std::string > > &dicts);
+void handleUnion(bsTree< std::string, bsTree< long long, std::string > > &dicts);
 void displayWarning(std::ostream &out, const std::string &message);
 
 #endif
