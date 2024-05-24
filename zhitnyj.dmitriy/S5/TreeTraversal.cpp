27aee34657e6eb0e0bfe2f1abd7c81ca6d48129e
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-05-24 11:56:07 +0300

    Add files via upload

---
+++ zhitnyj.dmitriy/S5/TreeTraversal.cpp
@@ -0,0 +1,103 @@
+#include "TreeTraversal.hpp"
+#include <iostream>
+
+void traverseAndPrintAscending(const BSTree< long long, std::string > &tree) {
+  if (tree.empty()) {
+    std::cout << "<EMPTY>\n";
+    return;
+  }
+
+  long long keySum = 0;
+
+  auto f = [&](const std::pair< const long long, std::string > &kv) {
+    keySum += kv.first;
+  };
+
+  tree.traverse_lnr(f);
+
+  std::cout << keySum;
+  bool first = true;
+
+  auto print = [&](const std::pair< const long long, std::string > &kv) {
+    if (first) {
+      std::cout << " " << kv.second;
+      first = false;
+    }
+    else {
+      std::cout << " " << kv.second;
+    }
+  };
+
+  tree.traverse_lnr(print);
+  std::cout << "\n";
+}
+
+void traverseAndPrintDescending(const BSTree< long long, std::string > &tree) {
+  if (tree.empty()) {
+    std::cout << "<EMPTY>\n";
+    return;
+  }
+
+  long long keySum = 0;
+
+  auto f = [&](const std::pair< const long long, std::string > &kv) {
+    keySum += kv.first;
+  };
+
+  tree.traverse_rnl(f);
+
+  std::cout << keySum;
+  bool first = true;
+
+  auto print = [&](const std::pair< const long long, std::string > &kv) {
+    if (first) {
+      std::cout << " " << kv.second;
+      first = false;
+    }
+    else {
+      std::cout << " " << kv.second;
+    }
+  };
+
+  tree.traverse_rnl(print);
+  std::cout << "\n";
+}
+
+void traverseAndPrintBreadth(const BSTree< long long, std::string > &tree) {
+  if (tree.empty()) {
+    std::cout << "<EMPTY>\n";
+    return;
+  }
+
+  long long keySum = 0;
+
+  auto f = [&](const std::pair< const long long, std::string > &kv) {
+    keySum += kv.first;
+  };
+
+  tree.traverse_breadth(f);
+
+  std::cout << keySum;
+  bool first = true;
+
+  auto print = [&](const std::pair< const long long, std::string > &kv) {
+    if (first) {
+      std::cout << " " << kv.second;
+      first = false;
+    }
+    else {
+      std::cout << " " << kv.second;
+    }
+  };
+
+  tree.traverse_breadth(print);
+  std::cout << "\n";
+}
+
+void emptyCommandWarning(std::ostream &out) {
+  out << "<EMPTY>\n";
+}
+
+void invalidCommandWarning(std::ostream &out) {
+  out << "<INVALID COMMAND>\n";
+}
---
+++ zhitnyj.dmitriy/S5/TreeTraversal.hpp
@@ -0,0 +1,12 @@
+#ifndef TREETRAVERSAL_HPP
+#define TREETRAVERSAL_HPP
+
+#include "BinarySearchTree.hpp"
+
+void traverseAndPrintAscending(const BSTree< long long, std::string > &tree);
+void traverseAndPrintDescending(const BSTree< long long, std::string > &tree);
+void traverseAndPrintBreadth(const BSTree< long long, std::string > &tree);
+void invalidCommandWarning(std::ostream &out);
+void emptyCommandWarning(std::ostream &out);
+
+#endif
