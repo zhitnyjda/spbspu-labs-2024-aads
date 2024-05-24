94d64200714835809d3400d6e0573d6544f129a3
harduserNIKITA <cumpotttt@yandex.ru>
2024-05-24 16:59:04 +0300

    добавил операции инкремента к Iterator

--- kovshikov.nikita/S4/BinarySearchTree.hpp
+++ kovshikov.nikita/S4/BinarySearchTree.hpp
@@ -1,17 +1,22 @@
 #ifndef BINARYSEARCHTREE_HPP
 #define BINARYSEARCHTREE_HPP
 #include <utility>
+#include <functional>
+#include <iterator>
 
 namespace kovshikov
 {
-  template< typename Key, typename Value, typename Compare >
+  template< typename Key, typename Value, typename Compare = std::less< Key > >
  // class BinarySearchTree
   class Tree
   {
   public:
     class Node;
-    using pair = std::pair< Key, Value >;
+    class Iterator;
+    class ConstIterator;
 
+    using pair = std::pair< Key, Value >;
+    Compare comp;
   private:
     *Node root_;
   }
@@ -41,5 +46,59 @@ kovshikov::Tree< Key, Value, Compare >::Node::Node(Key key, Value value, *Node f
   height_(height),
   element_(std::make_pair(key, value))
 {};
+
+template< typename Key, typename Value, typename Compare >
+kovshikov::Tree< Key, Value, Compare >::Iterator : public std::iterator< std::bidirectional_iterator_tag, pair >
+{
+public:
+  friend class Tree< Key, Value, Compare >;
+  using this_t = Iterator;
+
+  Iterator(): node_(nullptr), root_(nullptr) {};
+  Iterator(const this_t&) = default;
+  ~Iterator() = default;
+
+  this_t& operator=(const this_t &) = default;
+
+  this_t& operator++();
+  this_t operator++(int);
+private:
+  *Node node_;
+  *Node root_;
+}
+
+template< typename Key, typename Value, typename Compare >
+kovshikov::Tree< Key, Value, Compare >::Iterator::this_t& kovshikov::Tree< Key, Value, Compare >::Iterator::operator++()
+{
+  Node* current = node;
+  if(current -> right)
+  {
+    current = current -> right;
+    while(current -> left)
+    {
+      current = current -> left;
+    }
+  }
+  else
+  {
+    Node* currentFather = current -> father;
+    while(currentFather && current == currentFather -> right)
+    {
+      current = currentFather;
+      currentFather = currentFather -> father;
+    }
+    current = currentFather;
+  }
+  node = current;
+  return *this;
+}
+
+template< typename Key, typename Value, typename Compare >
+kovshikov::Tree< Key, Value, Compare >::Iterator::this_t& kovshikov::Tree< Key, Value, Compare >::Iterator::operator++(int)
+{
+  this_t currentIterator = *this;
+  ++(*this)
+  return currentIterator;
+}
 
 #endif
