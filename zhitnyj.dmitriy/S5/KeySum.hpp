48bab126543f3955bda32e980046153c66338847
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-05-24 13:44:43 +0300

    Update KeySum.hpp

--- zhitnyj.dmitriy/S5/KeySum.hpp
+++ zhitnyj.dmitriy/S5/KeySum.hpp
@@ -9,8 +9,10 @@ struct KeySum {
   void operator()(const std::pair< const long long, std::string > &key_value) {
     std::cout << std::numeric_limits< long long >::max();
     queue.push(key_value);
-    if ((result_ > std::numeric_limits< long long >::max() - key_value.first) ||
-        (key_value.first < 0 && result_ < std::numeric_limits< long long >::min() - key_value.first)) {
+    bool isOverflow = result_ > std::numeric_limits< long long >::max() - key_value.first;
+    bool isUndeflow = (key_value.first < 0) && (result_ < std::numeric_limits< long long >::min() - key_value.first);
+
+    if (isOverflow || isUndeflow) {
       throw std::overflow_error("Key sum overflow");
     }
     result_ += key_value.first;
