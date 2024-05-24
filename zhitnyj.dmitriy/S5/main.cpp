903da09cdadff3f41811f07174f7108a850b7d73
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-05-24 11:55:46 +0300

    Create main.cpp

---
+++ zhitnyj.dmitriy/S5/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <functional>
+#include <limits>
+#include <fstream>
+#include "BinarySearchTree.hpp"
+#include "TreeTraversal.hpp"
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Wrong input\n";
+    return 1;
+  }
+
+  BSTree< long long, std::string > dictionaries;
+  BSTree< std::string, std::function< void(BSTree< long long, std::string > &) > > cmds;
+
+  cmds.insert(std::make_pair("ascending", traverseAndPrintAscending));
+  cmds.insert(std::make_pair("descending", traverseAndPrintDescending));
+  cmds.insert(std::make_pair("breadth", traverseAndPrintBreadth));
+
+  try {
+    std::ifstream file(argv[1]);
+    if (!file) {
+      throw std::runtime_error("Unable to open file");
+    }
+
+    long long key;
+    std::string value;
+    while (file >> key >> value) {
+      dictionaries.push(key, value);
+    }
+  } catch (const std::exception &e) {
+    std::cerr << "Error loading file: " << e.what() << "\n";
+    return 1;
+  }
+
+  while (!std::cin.eof()) {
+    std::string command;
+    std::cin >> command;
+
+    auto commandIter = cmds.find(command);
+    if (commandIter != cmds.end() && !command.empty()) {
+      commandIter->second(dictionaries);
+    }
+    else if (!command.empty()) {
+      invalidCommandWarning(std::cout);
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
+  return 0;
+}
