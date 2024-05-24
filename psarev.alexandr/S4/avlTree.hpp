a4d1d53248784154d5993cb78495c726d41832a6
Александр Псарев <alexandrpsarev5555@gmail.com>
2024-05-24 18:41:07 +0300

    fixed initialization order

--- psarev.alexandr/S4/avlTree.hpp
+++ psarev.alexandr/S4/avlTree.hpp
@@ -55,7 +55,7 @@ namespace psarev
       Unit* right;
       size_t hgt;
 
-      Unit(dataType data_, Unit* ancest_ = nullptr, int hgt_ = 0, Unit* left_ = nullptr, Unit* right_ = nullptr) :
+      Unit(dataType data_, Unit* ancest_ = nullptr, Unit* left_ = nullptr, Unit* right_ = nullptr, int hgt_ = 0) :
         data(data_),
         ancest(ancest_),
         hgt(hgt_),
