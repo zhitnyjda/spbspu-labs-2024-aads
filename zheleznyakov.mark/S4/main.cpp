1dbdeeeb132bc31b730b4213890a7cf2ad6f9f8b
Mark Zheleznyakov <mrqiz@internet.ru>
2024-05-25 18:00:24 +0300

    feat(S4): Методы at и push дерева

--- zheleznyakov.mark/S4/main.cpp
+++ zheleznyakov.mark/S4/main.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
+#include <string>
 #include "tree.hpp"
 
 int main()
 {
+  using namespace zheleznyakov;
+  zheleznyakov::Tree< std::string, int > tree;
   return 0;
 }
--- zheleznyakov.mark/S4/tree.hpp
+++ zheleznyakov.mark/S4/tree.hpp
@@ -10,8 +10,12 @@ namespace zheleznyakov {
   public:
     using data_t = typename std::pair< Key, Value >;
 
-    Tree() = default;
+    Tree();
     ~Tree() = default;
+
+    Value at(const Key & k);
+
+    void push(const Key &, const Value &);
   private:
     struct Node
     {
@@ -33,5 +37,73 @@ namespace zheleznyakov {
 
     Node * root_;
   };
+}
+
+template < typename Key, typename Value, typename Compare >
+zheleznyakov::Tree< Key, Value, Compare >::Tree():
+  root_{nullptr}
+{}
+
+template < typename Key, typename Value, typename Compare >
+Value zheleznyakov::Tree< Key, Value, Compare >::at(const Key & k) {
+  Node* current = root_;
+  Compare cmp;
+
+  while (current != nullptr)
+  {
+    if (cmp(current->data.first, k))
+    {
+      current = current->right;
+    }
+    else if (cmp(k, current->data.first))
+    {
+      current = current->left;
+    }
+    else
+    {
+      return current->data.second;
+    }
+  }
+  throw std::out_of_range("No such key");
+}
+
+template <typename Key, typename Value, typename Compare>
+void zheleznyakov::Tree<Key, Value, Compare>::push(const Key & k, const Value & v) {
+  Node* new_node = new Node();
+  new_node->data = std::make_pair(k, v);
+
+  Compare cmp;
+
+  if (root_ == nullptr)
+  {
+    root_ = new_node;
+    return;
+  }
+
+  Node* current = root_;
+  Node* parent = nullptr;
+
+  while (current != nullptr)
+  {
+    parent = current;
+    if (cmp(k, current->data.first))
+    {
+      current = current->left;
+    }
+    else
+    {
+      current = current->right;
+    }
+  }
+
+  new_node->parent = parent;
+  if (cmp(k, parent->data.first))
+  {
+    parent->left = new_node;
+  }
+  else
+  {
+    parent->right = new_node;
+  }
 }
 #endif
