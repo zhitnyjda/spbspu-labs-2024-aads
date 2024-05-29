bfb988c8c7861b90746ad1ce9c67a89b506df348
Vlad0sEnIgma345 <vlad0senigma345@gmail.com>
2024-05-29 23:16:13 +0300

    feat(S4): Добавлены конструкторы копирования, присваивания, деструктор и тд и тп, т.е. пока что все без методов

--- mihalchenko.vladislav/S4/AVLtree.hpp
+++ mihalchenko.vladislav/S4/AVLtree.hpp
@@ -33,24 +33,6 @@ namespace mihalchenko
     AVLTree(AVLTree &&move);
     ~AVLTree();
 
-    void insert(Key key, Value value);
-    using value_std_pair = std::pair<const Key, Value>;
-    void insert(std::pair<const Key, Value> pairKeyVal);
-    bool empty() const noexcept;
-    size_t getSize() const noexcept;
-    size_t erase(const Key &key);
-    void clear();
-
-    template <class... Args>
-    void emplace(Args &&...args);
-
-    Value &operator[](const Key &key);
-    Iterator find(const Key &key);
-    ConstIterator cbegin() const noexcept;
-    ConstIterator cend() const noexcept;
-    Iterator begin() noexcept;
-    Iterator end() noexcept;
-
   private:
     class Node
     {
@@ -65,67 +47,70 @@ namespace mihalchenko
       Node *right_;
       Node *previous_;
     };
-
-    Node *insertNode(Key k, Value v, Node *node, Node *prev);
-    Iterator findNode(const Key &key, Node *node);
-
-    void leftSpin(Node *node);
-    void leftRightSpin(Node *node);
-    void rightSpin(Node *node);
-    void rightLeftSpin(Node *node);
-
-    int calcHeight(Node *node);
-    Node *SearchHiHeight(Node *node);
-
-    Node *copyTree(Node *node, Node *previous);
-    void doStableTree(Node *checkNode);
-    void clear(Node *node);
-    void recombinationLinks(Node *deletedNode, Node *prevDelNode);
-
     Node *root_;
     size_t size_ = 0;
   };
-}
-
-template <typename Key, typename Value, typename Compare>
-class mihalchenko::AVLTree<Key, Value, Compare>::ConstIterator : public std::iterator<std::bidirectional_iterator_tag, std::pair<const Key, Value>>
-// https://learn.microsoft.com/ru-ru/cpp/standard-library/bidirectional-iterator-tag-struct?view=msvc-170
-{
-public:
-  friend class AVLTree<Key, Value, Compare>;
-  ConstIterator();
-  ConstIterator(Node *node, Node *root);
-  ConstIterator(const ConstIterator &) = default;
-  ~ConstIterator() = default;
-
-  ConstIterator &operator++();
-  ConstIterator operator++(int);
-  ConstIterator &operator--();
-  ConstIterator operator--(int);
-
-  const std::pair<const Key, Value> &operator*() const;
-  const std::pair<const Key, Value> *operator->() const;
-
-  bool operator!=(const ConstIterator &rhs) const;
-  bool operator==(const ConstIterator &rhs) const;
-
-  ConstIterator &operator=(const ConstIterator &) = default;
-
-private:
-  Node *node_;
-  Node *root_;
 };
 
-template <typename Key, typename Value, typename Compare>
-class mihalchenko::AVLTree<Key, Value, Compare>::Iterator : public std::iterator<std::bidirectional_iterator_tag, Key, Value, Compare>
-{
-public:
-  friend class AVLTree<Key, Value, Compare>;
-  Iterator();
-};
 
 template <typename Key, typename Value, typename Compare>
 mihalchenko::AVLTree<Key, Value, Compare>::Node::Node(Key key, Value data, int height, Node *left, Node *right, Node *previous) : pairOfKeyVal(std::make_pair(key, data)), height_(height), left_(left), right_(right), previous_(previous)
 {
 }
+
+template <typename Key, typename Value, typename Compare>
+mihalchenko::AVLTree<Key, Value, Compare>::AVLTree() : root_(nullptr), size_(0)
+{
+}
+
+template <typename Key, typename Value, typename Compare>
+mihalchenko::AVLTree<Key, Value, Compare>::AVLTree(size_t count, std::pair<const Key, Value> &pairKeyVal) : root_(nullptr), size_(0)
+{
+  for (size_t i = 0; i < count; ++i)
+  {
+    insert(pairKeyVal);
+  }
+}
+
+template <typename Key, typename Value, typename Compare>
+template <class InputIt>
+mihalchenko::AVLTree<Key, Value, Compare>::AVLTree(InputIt start, InputIt stop)
+{
+  for (InputIt it = start; it != stop; ++it)
+  {
+    insert(*it);
+  }
+}
+
+template <typename Key, typename Value, typename Compare>
+mihalchenko::AVLTree<Key, Value, Compare>::AVLTree(std::initializer_list<std::pair<const Key, Value>> initAVL) : root_(nullptr), size_(0)
+{
+  for (auto it = initAVL.begin(); it != initAVL.end(); ++it)
+  {
+    insert(*it);
+  }
+}
+
+template <typename Key, typename Value, typename Compare>
+mihalchenko::AVLTree<Key, Value, Compare>::AVLTree(const AVLTree &copy) : root_(nullptr), size_(copy.size_)
+{
+  root_ = copyTree(copy.root_, nullptr);
+}
+
+template <typename Key, typename Value, typename Compare>
+mihalchenko::AVLTree<Key, Value, Compare>::AVLTree(AVLTree &&move) : root_(nullptr), size_(std::move(move.size_))
+{
+  root_ = copyTree(std::move(move.root_), nullptr);
+  move.clear();
+  move.root_ = nullptr;
+  move.size_ = 0;
+}
+
+template <typename Key, typename Value, typename Compare>
+mihalchenko::AVLTree<Key, Value, Compare>::~AVLTree()
+{
+  clear();
+  root_ = nullptr;
+}
+
 #endif
