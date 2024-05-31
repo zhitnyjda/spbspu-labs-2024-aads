96dccf59962d47979e8c3ab4f33503475ee7bddc
anyadg <anna_giraffe@mail.ru>
2024-05-31 21:10:53 +0300

    added out of range error

--- doroshenko.anna/S5/KeyValSumm.cpp
+++ doroshenko.anna/S5/KeyValSumm.cpp
@@ -1,4 +1,6 @@
 #include "KeyValSumm.hpp"
+#include <limits>
+#include <stdexcept>
 
 doroshenko::KeyValSumm::KeyValSumm() :
   keySum_(0),
@@ -7,6 +9,12 @@ doroshenko::KeyValSumm::KeyValSumm() :
 
 void doroshenko::KeyValSumm::operator()(const std::pair< const int, std::string >& keyval)
 {
+  int maximum = std::numeric_limits< int >::max();
+  int minimum = std::numeric_limits< int >::min();
+  if (maximum - keyval.first < keySum_ || (keyval.first < 0 && minimum - keyval.first > keySum_))
+  {
+    throw std::out_of_range("<OUT OF RANGE>\n");
+  }
   keySum_ += keyval.first;
   if (!valSum_.empty())
   {
