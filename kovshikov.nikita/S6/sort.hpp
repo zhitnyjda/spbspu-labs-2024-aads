56e1fdf781a570823b5c7ae15f2d06111b1aa10c
harduserNIKITA <cumpotttt@yandex.ru>
2024-06-01 19:13:54 +0300

    add selectionSort

---
+++ kovshikov.nikita/S6/sort.hpp
@@ -0,0 +1,36 @@
+#ifndef SORT_HPP
+#define SORT_HPP
+#include <utility> //swap
+//#include <algorithm> //iter_swap
+#include "compare.hpp"
+
+namespace kovshikov
+{
+  //для std::forward_list
+  template< typename FwdIterator, typename Compare >
+  void selectionSort(FwdIterator begin, FwdIterator end, Compare comp);
+}
+
+template< typename FwdIterator, typename Compare >
+void kovshikov::selectionSort(FwdIterator begin, FwdIterator end, Compare comp)
+{
+  while(begin != end)
+  {
+    FwdIterator extreme = begin; //extreme
+    FwdIterator current = begin;
+    while(current != end)
+    {
+      if(comp(*current, *extreme)
+      {
+        extreme = current;
+      }
+      current++;
+    }
+    std::swap(*extreme, *begin);
+  //std::iter_sawp(extreme, current);
+    begin++;
+  }
+}
+
+#endif
+
