8207f00c2ea7329815c9da64781a087a5b8cbea6
amotvei <motya2228@mail.ru>
2024-06-03 00:37:17 +0300

    start of start

---
+++ taskaev.matvei/S4/functions.hpp
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+namespace taskaev
+{
+  // later
+}
+
+#endif
--- taskaev.matvei/S4/main.cpp
+++ taskaev.matvei/S4/main.cpp
@@ -1,6 +1,11 @@
-#include <iosteeam>
+#include <string>
+#include <fstream>
+#include <limits>
+#include "BSTree.hpp"
+#include "functions.hpp"
+using namespace taskaev;
 
-int main()
+int main(int argc, char ** argv)
 {
   return 0;
 }
