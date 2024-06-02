0e2b3df3e130cb7531188c666c86b51ff78a3233
harduserNIKITA <cumpotttt@yandex.ru>
2024-06-02 14:38:05 +0300

    пытаюсь написать сортировку Шелла

--- kovshikov.nikita/S6/main.cpp
+++ kovshikov.nikita/S6/main.cpp
@@ -1,5 +1,7 @@
+#include <deque>
 #include <iostream>
 #include <forward_list>
+#include <list>
 #include "sort.hpp"
 
 int main()
@@ -15,4 +17,25 @@ int main()
     std::cout << *iterator << " ";
     iterator++;
   }
+  std::deque< int > dint{1, 6, 10, -6, 3, 0, -4, 11, 2};
+  Descending< int > descending2;
+  selectionSort(dint.begin(), dint.end(), descending2);
+  auto iterator2 = dint.begin();
+  while(iterator2 != dint.end())
+  {
+    std::cout << *iterator2 << " ";
+    iterator2++;
+  }
+
+  std::list< int > flint2{1, 6, 10, -6, 3, 0, -4, 11, 2};
+  Ascending< int > ascending;
+  //Descending< int > descending;
+  shellSort(flint2.begin(), flint2.end(), ascending);
+  auto iterator3 = flint2.begin();
+  while(iterator3 != flint2.end())
+  {
+    std::cout << *iterator3 << " ";
+    iterator3++;
+  }
+
 }
--- kovshikov.nikita/S6/sort.hpp
+++ kovshikov.nikita/S6/sort.hpp
@@ -2,6 +2,7 @@
 #define SORT_HPP
 #include <utility> //swap
 //#include <algorithm> //iter_swap
+#include <iterator> //std::distance
 #include "compare.hpp"
 
 namespace kovshikov
@@ -9,6 +10,9 @@ namespace kovshikov
   //для std::forward_list
   template< typename FwdIterator, typename Compare >
   void selectionSort(FwdIterator begin, FwdIterator end, Compare comp);
+
+  template< typename ListIterator, typename Compare >
+  void shellSort(ListIterator begin, ListIterator end, Compare comp);
 }
 
 template< typename FwdIterator, typename Compare >
@@ -31,6 +35,35 @@ void kovshikov::selectionSort(FwdIterator begin, FwdIterator end, Compare comp)
     begin++;
   }
 }
+
+template< typename ListIterator, typename Compare >
+void kovshikov::shellSort(ListIterator begin, ListIterator end, Compare comp)
+{
+  size_t size = std::distance(begin, end);
+  for(size_t interval = size / 2; interval > 0; interval /= 2)
+  {
+    //интревал = количеству подсписков
+    for(size_t i = 0; i < interval; i++)
+    {
+      ListIterator current = std::next(begin, i); //begin + i;
+      ListIterator next = std::next(current, interval); //current + interval;
+      while(next != std::next(end, i)) //+ i) //тут возможно с адресной арифметикой что то
+      {
+        ListIterator prev = std::prev(next, interval); //next - interval;
+        while(prev != std::next(begin, i - interval)) //)end)//begin + i - interval)
+        {
+          if(comp(*next, *prev))
+          {
+            std::swap(*next, *prev);
+          }
+          prev = std::prev(prev, interval);  //prev - interval;
+        }
+        next = std::next(next, interval); //next + interval;
+      }
+    }
+  }
+}
+//у итераторов нет operator++
 
 #endif
 
