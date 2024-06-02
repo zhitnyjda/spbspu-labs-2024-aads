b269dc5c0966540d631cd85e0c23b536e7615b3c
ElectronixTM <etdiary1@gmail.com>
2024-06-02 11:59:03 +0300

    feat(S6): реализован метод заполнения целочисленных массивов

--- miheev.egor/S6/IOProcessing.hpp
+++ miheev.egor/S6/IOProcessing.hpp
@@ -2,6 +2,7 @@
 #define IO_PROCESSING_HPP
 
 #include <iostream>
+#include <iomanip>
 #include <deque>
 #include <list>
 #include <list.hpp>
@@ -11,17 +12,17 @@ namespace miheev
   template< typename Container >
   void printContainer(std::ostream&, const Container&);
 
+  void fillContainersWithRandom(size_t size, List< int >&, std::list< int >&, std::deque< int >&);
+  void fillContainersWithRandom(std::deque< int >&, std::list< int >&, List< int >&, size_t amount);
+  
   template< typename T, typename Comparator >
   void testSorts(std::ostream&, size_t size,  Comparator comp);
-
-  void fillContainersWithRandom(size_t size, List< int >&, std::list< int >&, std::deque< int >&)
-  void fillContainersWithRandom(size_t size, List< double >&, std::list< double >&, std::deque< double >&)
 }
 
 template< typename Container >
 void miheev::printContainer(std::ostream& out, const Container& container)
 {
-  out << std::fixed() << std::setprecision(1);
+  out << std::fixed << std::setprecision(1);
   for (auto iter(container.begin()); iter != container.end();)
   {
     out << *iter;
@@ -32,7 +33,16 @@ void miheev::printContainer(std::ostream& out, const Container& container)
   }
 }
 
-
+void miheev::fillContainersWithRandom(std::deque< int >& deque, std::list< int >& bidirect, List< int >& forward, size_t amount)
+{
+  for (size_t i = 0; i < amount; i++)
+  {
+    int random = rand() % 100;
+    deque.push_back(random);
+  }
+  std::copy(deque.begin(), deque.end(), std::back_inserter(forward));
+  std::copy(deque.begin(), deque.end(), std::back_inserter(bidirect));
+}
 
 #endif
 
--- miheev.egor/S6/main.cpp
+++ miheev.egor/S6/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "sorts.hpp"
 
-int main(int argc, char* argv[])
+int main()
 {
   using namespace miheev;
 
