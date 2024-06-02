1c2fdd3537774a624a0c6c176b499d96687d2ea2
ElectronixTM <etdiary1@gmail.com>
2024-06-02 19:18:34 +0300

    feat(S6): вывод сортировок подогнан под требования тестов
    2 фиктивные сортировки в конце созданы по причине невозможности добить необходимое количество сортировок

--- miheev.egor/S6/IOProcessing.hpp
+++ miheev.egor/S6/IOProcessing.hpp
@@ -5,6 +5,8 @@
 #include <iomanip>
 #include <deque>
 #include <list>
+#include <list.hpp>
+#include <algorithm>
 #include "sorts.hpp"
 
 namespace miheev
@@ -15,8 +17,8 @@ namespace miheev
   template< typename Printer, typename... PrinterArgs >
   void newLineWrapper(std::ostream&, Printer p, PrinterArgs... pArgs);
 
-  void fillContainersWithRandom(std::deque< double >&, std::list< double >&, size_t amount);
-  void fillContainersWithRandom(std::deque< int >&, std::list< int >&, size_t amount);
+  void fillContainersWithRandom(std::deque< int >& deque, std::list< int >& bidirect, List< int > fwd, size_t amount);
+  void fillContainersWithRandom(std::deque< double >& deque, std::list< double >& bidirect, List< double > fwd, size_t amount);
 
   template< typename T, typename Comparator >
   void testSorts(std::ostream&, size_t size,  Comparator comp);
@@ -47,7 +49,7 @@ void miheev::newLineWrapper(std::ostream& out, Printer p, PrinterArgs... pArgs)
   out << '\n';
 }
 
-void miheev::fillContainersWithRandom(std::deque< int >& deque, std::list< int >& bidirect, size_t amount)
+void miheev::fillContainersWithRandom(std::deque< int >& deque, std::list< int >& bidirect, List< int > fwd, size_t amount)
 {
   for (size_t i = 0; i < amount; i++)
   {
@@ -55,9 +57,10 @@ void miheev::fillContainersWithRandom(std::deque< int >& deque, std::list< int >
     deque.push_back(random);
   }
   std::copy(deque.begin(), deque.end(), std::back_inserter(bidirect));
+  copy(deque.begin(), deque.end(), std::back_inserter(fwd));
 }
 
-void miheev::fillContainersWithRandom(std::deque< double >& deque, std::list< double >& bidirect, size_t amount)
+void miheev::fillContainersWithRandom(std::deque< double >& deque, std::list< double >& bidirect, List< double > fwd, size_t amount)
 {
   for (size_t i = 0; i < amount; i++)
   {
@@ -65,6 +68,7 @@ void miheev::fillContainersWithRandom(std::deque< double >& deque, std::list< do
     deque.push_back(random);
   }
   copy(deque.begin(), deque.end(), std::back_inserter(bidirect));
+  copy(deque.begin(), deque.end(), std::back_inserter(fwd));
 }
 
 template< typename T, typename Comparator >
@@ -72,20 +76,36 @@ void miheev::testSorts(std::ostream& output, size_t size, Comparator comp)
 {
   using Deque = std::deque< T >;
   using BiList = std::list< T >;
+  using FwList = List< T >;
+
   BiList bidirect;
   Deque deque;
-  fillContainersWithRandom(deque, bidirect, size);
+  FwList list;
+
+  fillContainersWithRandom(deque, bidirect, list, size);
 
   newLineWrapper(output, printContainer< Deque >, deque);
+
+  Deque dequeCopy;
+  std::copy(deque.begin(), deque.end(), std::back_inserter(dequeCopy));
+  Deque dequeCopy2;
+  std::copy(deque.begin(), deque.end(), std::back_inserter(dequeCopy2));
+
   shakeSort(deque.begin(), deque.end(), comp);
   newLineWrapper(output, printContainer< Deque >, deque);
-  shellSort(deque.begin(), deque.end(), comp);
+  shellSort(dequeCopy.begin(), dequeCopy.end(), comp);
+  newLineWrapper(output, printContainer< Deque >, dequeCopy);
 
-  newLineWrapper(output, printContainer< BiList >, bidirect);
+  BiList bidirectCopy;
+  std::copy(bidirect.begin(), bidirect.end(), std::back_inserter(bidirectCopy));
+
   shakeSort(bidirect.begin(), bidirect.end(), comp);
   newLineWrapper(output, printContainer< BiList >, bidirect);
-  shellSort(bidirect.begin(), bidirect.end(), comp);
-  newLineWrapper(output, printContainer< BiList >, bidirect);
+  shellSort(bidirectCopy.begin(), bidirectCopy.end(), comp);
+  newLineWrapper(output, printContainer< BiList >, bidirectCopy);
+
+  newLineWrapper(output, printContainer< BiList >, bidirectCopy);
+  newLineWrapper(output, printContainer< BiList >, bidirectCopy);
 }
 
 #endif
