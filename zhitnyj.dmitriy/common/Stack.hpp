01dd92a3152e133bd3a915ed3a941ede7245c20e
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-06-03 22:00:42 +0300

    Update Stack.hpp

--- zhitnyj.dmitriy/common/Stack.hpp
+++ zhitnyj.dmitriy/common/Stack.hpp
@@ -4,16 +4,16 @@
 #include <stdexcept>
 #include <List.hpp>
 
-template< typename T >
-class Stack
-{
-public:
+namespace zhitnyj {
+  template< typename T >
+  class Stack {
+  public:
     Stack() = default;
     ~Stack() = default;
-    Stack(const Stack& other) noexcept;
-    Stack(Stack&& other) noexcept;
-    Stack& operator=(const Stack& other);
-    Stack& operator=(Stack&& other) noexcept;
+    Stack(const Stack &other) noexcept;
+    Stack(Stack &&other) noexcept;
+    Stack &operator=(const Stack &other);
+    Stack &operator=(Stack &&other) noexcept;
 
     bool empty() const noexcept;
     size_t size() const noexcept;
@@ -22,99 +22,85 @@ public:
     void push_back(const T &value) noexcept;
     void pop();
 
-    T& top();
-    T& back();
+    T &top();
+    T &back();
 
-private:
-    List< T > list;
-};
+  private:
+    List <T> list;
+  };
 
-template< typename T >
-Stack< T >::Stack(const Stack& other) noexcept: list(other.list)
-{}
-
-template< typename T >
-Stack< T >::Stack(Stack&& other) noexcept : list(std::move(other.list))
-{}
-
-template< typename T >
-Stack< T >& Stack< T >::operator=(const Stack& other)
-{
-  if (this != &other)
-  {
-    list = other.list;
+  template< typename T >
+  Stack< T >::Stack(const Stack &other) noexcept: list(other.list) {
   }
-  return *this;
-}
 
-template< typename T >
-Stack< T >& Stack< T >::operator=(Stack&& other) noexcept
-{
-  if (this != &other)
-  {
-    list = std::move(other.list);
+  template< typename T >
+  Stack< T >::Stack(Stack &&other) noexcept : list(std::move(other.list)) {
   }
-  return *this;
-}
 
-template< typename T >
-void Stack< T >::push(const T &value) noexcept
-{
-  list.push_front(value);
-}
-
-template< typename T >
-void Stack< T >::push_back(const T &value) noexcept
-{
-  list.push_back(value);
-}
-
-template< typename T >
-void Stack< T >::pop()
-{
-  if (list.empty())
-  {
-    throw std::out_of_range("Stack is empty");
+  template< typename T >
+  Stack< T > &Stack< T >::operator=(const Stack &other) {
+    if (this != &other) {
+      list = other.list;
+    }
+    return *this;
   }
-  list.pop();
-}
 
-template< typename T >
-T& Stack< T >::top()
-{
-  if (list.empty())
-  {
-    throw std::out_of_range("Stack is empty");
+  template< typename T >
+  Stack< T > &Stack< T >::operator=(Stack &&other) noexcept {
+    if (this != &other) {
+      list = std::move(other.list);
+    }
+    return *this;
   }
-  return list[0];
-}
 
-template< typename T >
-T& Stack< T >::back()
-{
-  if (list.empty())
-  {
-    throw std::out_of_range("Stack is empty");
+  template< typename T >
+  void Stack< T >::push(const T &value) noexcept {
+    list.push_front(value);
   }
-  return list[list.size() - 1];
-}
 
-template< typename T >
-bool Stack< T >::empty() const noexcept
-{
-  return list.empty();
-}
+  template< typename T >
+  void Stack< T >::push_back(const T &value) noexcept {
+    list.push_back(value);
+  }
 
-template< typename T >
-void Stack< T >::clear() noexcept
-{
-  list.clear();
-}
+  template< typename T >
+  void Stack< T >::pop() {
+    if (list.empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    list.pop();
+  }
 
-template< typename T >
-size_t Stack< T >::size() const noexcept
-{
-  return list.size();
+  template< typename T >
+  T &Stack< T >::top() {
+    if (list.empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    return list[0];
+  }
+
+  template< typename T >
+  T &Stack< T >::back() {
+    if (list.empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    return list[list.size() - 1];
+  }
+
+  template< typename T >
+  bool Stack< T >::empty() const noexcept {
+    return list.empty();
+  }
+
+  template< typename T >
+  void Stack< T >::clear() noexcept {
+    list.clear();
+  }
+
+  template< typename T >
+  size_t Stack< T >::size() const noexcept {
+    return list.size();
+  }
 }
 
 #endif
