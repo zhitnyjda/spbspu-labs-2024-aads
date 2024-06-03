a375f6d2b3c9734ecbc4ce61db8886392750e379
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-06-03 21:47:04 +0300

    Update SequenceHandler.hpp

--- zhitnyj.dmitriy/S1/SequenceHandler.hpp
+++ zhitnyj.dmitriy/S1/SequenceHandler.hpp
@@ -5,19 +5,19 @@
 #include <List.hpp>
 
 namespace zhitnyj {
-class SequenceHandler {
-public:
-  SequenceHandler() = default;
+  class SequenceHandler {
+  public:
+    SequenceHandler() = default;
 
-  void addSequence(const std::string &name, const List< unsigned long long > &sequence);
-  void calculateSums();
-  void printSequences();
-  void printSequencesNames();
+    void addSequence(const std::string &name, const List< unsigned long long > &sequence);
+    void calculateSums();
+    void printSequences();
+    void printSequencesNames();
 
-private:
-  List< std::string > names;
-  List< List< unsigned long long > > sequences;
-};
+  private:
+    List < std::string > names;
+    List < List< unsigned long long > > sequences;
+  };
 }
 
 #endif
