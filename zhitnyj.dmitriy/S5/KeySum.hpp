53366b32a9ff0678ef8fc01d79de5807c1f07e3b
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-06-03 22:01:15 +0300

    Update KeySum.hpp

--- zhitnyj.dmitriy/S5/KeySum.hpp
+++ zhitnyj.dmitriy/S5/KeySum.hpp
@@ -5,23 +5,25 @@
 #include <stdexcept>
 #include <Queue.hpp>
 
-struct KeySum {
-  void operator()(const std::pair< const long long, std::string > &key_value) {
-    queue.push(key_value);
-    bool isOverflow = result_ > std::numeric_limits< long long >::max() - key_value.first;
-    bool isUndeflow = (key_value.first < 0) && (result_ < std::numeric_limits< long long >::min() - key_value.first);
+namespace zhitnyj {
+  struct KeySum {
+    void operator()(const std::pair< const long long, std::string > &key_value) {
+      queue.push(key_value);
+      bool isOverflow = result_ > std::numeric_limits< long long >::max() - key_value.first;
+      bool isUndeflow = (key_value.first < 0) && (result_ < std::numeric_limits< long long >::min() - key_value.first);
 
-    if (isOverflow) {
-      throw std::overflow_error("Key sum overflow!");
+      if (isOverflow) {
+        throw std::overflow_error("Key sum overflow!");
+      }
+      else if (isUndeflow) {
+        throw std::overflow_error("Key sum undeflow!");
+      }
+      result_ += key_value.first;
     }
-    else if (isUndeflow) {
-      throw std::overflow_error("Key sum undeflow!");
-    }
-    result_ += key_value.first;
-  }
 
-  long long result_ = 0;
-  Queue< std::pair< const long long, std::string>> queue;
-};
+    long long result_ = 0;
+    Queue <std::pair< const long long, std::string>> queue;
+  };
+}
 
 #endif
