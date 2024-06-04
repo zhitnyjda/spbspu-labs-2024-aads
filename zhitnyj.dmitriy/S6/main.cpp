97f616d1ce394346ad73b607494807ab663b7f57
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-06-04 22:06:48 +0300

    Update main.cpp

--- zhitnyj.dmitriy/S6/main.cpp
+++ zhitnyj.dmitriy/S6/main.cpp
@@ -13,29 +13,34 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  bool ascending = std::string(argv[1]) == "ascending";
-  bool isInt = std::string(argv[2]) == "ints";
-  size_t size = std::stoi(argv[3]);
+  try {
+    bool ascending = std::string(argv[1]) == "ascending";
+    bool isInt = std::string(argv[2]) == "ints";
+    size_t size = std::stoi(argv[3]);
 
-  if (size < 1)
-  {
-    std::cerr << "Size must be bigger then 0!\n";
+    if (size < 1) {
+      std::cerr << "Size must be bigger then 0!\n";
+      return 1;
+    }
+
+    if (isInt) {
+      zhitnyj::List< int > singleList;
+      std::list< int > doubleList;
+      std::deque< int > deq;
+      zhitnyj::generateNumbers(singleList, doubleList, deq, size, true);
+      zhitnyj::performSorts(singleList, doubleList, deq, ascending, std::cout);
+    }
+    else {
+      zhitnyj::List< float > singleList;
+      std::list< float > doubleList;
+      std::deque< float > deq;
+      zhitnyj::generateNumbers(singleList, doubleList, deq, size, false);
+      zhitnyj::performSorts(singleList, doubleList, deq, ascending, std::cout);
+    }
+  }
+  catch (const std::exception &e) {
+    std::cerr << e.what() << "\n";
     return 1;
-  }
-
-  if (isInt) {
-    zhitnyj::List< int > singleList;
-    std::list< int > doubleList;
-    std::deque< int > deq;
-    zhitnyj::generateNumbers(singleList, doubleList, deq, size, true);
-    zhitnyj::performSorts(singleList, doubleList, deq, ascending, std::cout);
-  }
-  else {
-    zhitnyj::List< float > singleList;
-    std::list< float > doubleList;
-    std::deque< float > deq;
-    zhitnyj::generateNumbers(singleList, doubleList, deq, size, false);
-    zhitnyj::performSorts(singleList, doubleList, deq, ascending, std::cout);
   }
 
   return 0;
