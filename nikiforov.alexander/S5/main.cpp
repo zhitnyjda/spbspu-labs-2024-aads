3faa953f1c454924ec0eafca1ff87e5e7fec71cb
AlexNik <sahan@DESKTOP-R8OTJOJ>
2024-06-04 22:45:52 +0300

    fixed breadth method

--- nikiforov.alexander/S5/main.cpp
+++ nikiforov.alexander/S5/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
   {
     cmds.emplace("ascending", nikiforov::lnrTraverse);
     cmds.emplace("descending", nikiforov::rnlTraverse);
-    cmds.emplace("bradth", nikiforov::breadthTraverse);
+    cmds.emplace("breadth", nikiforov::breadthTraverse);
   }
 
   try
