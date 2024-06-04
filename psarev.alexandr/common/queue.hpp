ee292cf898d63d59fc010d531142a64add4acb9d
Александр Псарев <alexandrpsarev5555@gmail.com>
2024-06-04 17:06:42 +0300

    one damned space removed

--- psarev.alexandr/common/queue.hpp
+++ psarev.alexandr/common/queue.hpp
@@ -48,7 +48,7 @@ psarev::Queue< T >& psarev::Queue< T >::operator=(const Queue& other)
   depot = other.dep;
 }
 
-template< typename T >
+template < typename T >
 T& psarev::Queue< T >::getFront() const
 {
   return depot.getFront();
