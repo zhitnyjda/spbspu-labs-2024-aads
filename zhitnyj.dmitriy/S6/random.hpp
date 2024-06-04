2d2180705aee1ad60250a34fe1c6dcf516fbc72b
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-06-04 21:01:17 +0300

    Add files via upload

---
+++ zhitnyj.dmitriy/S6/random.hpp
@@ -0,0 +1,44 @@
+#ifndef RANDOM_HPP
+#define RANDOM_HPP
+
+#include <list>
+#include <algorithm>
+#include "List.hpp"
+
+namespace zhitnyj {
+  int generateRandomInt(int min, int max);
+  float generateRandomFloat(float min, float max);
+  template< typename T >
+  void generateNumbers(zhitnyj::List< T > &singleList, std::list< T > &doubleList, size_t size, bool isInt);
+}
+
+int zhitnyj::generateRandomInt(int min, int max) {
+  return min + std::rand() % (max - min + 1);
+}
+
+float zhitnyj::generateRandomFloat(float min, float max) {
+  return min + static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX) * (max - min);
+}
+
+template< typename T >
+void zhitnyj::generateNumbers(zhitnyj::List< T > &singleList, std::list< T > &doubleList, size_t size, bool isInt) {
+  std::srand(static_cast<unsigned int>(std::time(0)));
+
+  if (isInt) {
+    for (size_t i = 0; i < size; ++i) {
+      int num = generateRandomInt(0, 100);
+      singleList.push_back(num);
+      doubleList.push_back(num);
+    }
+  }
+  else {
+    for (size_t i = 0; i < size; ++i) {
+      float num = generateRandomFloat(0.0f, 100.0f);
+      singleList.push_back(num);
+      doubleList.push_back(num);
+    }
+  }
+}
+
+
+#endif
---
+++ zhitnyj.dmitriy/S6/sortOperations.hpp
@@ -0,0 +1,102 @@
+#ifndef SORTOPERATIONS_HPP
+#define SORTOPERATIONS_HPP
+
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include "List.hpp"
+#include "random.hpp"
+
+namespace zhitnyj {
+  template< typename T >
+  void printList(const List< T > &list, std::ostream &os) {
+    for (auto it = list.begin(); it != list.end(); ++it) {
+      os << *it << " ";
+    }
+    os << std::endl;
+  }
+
+  template< typename T >
+  void printStdList(const std::list< T > &list, std::ostream &os) {
+    for (const auto &elem: list) {
+      os << elem << " ";
+    }
+    os << std::endl;
+  }
+
+  template< typename forwardIterator, typename Compare >
+  void oddEvenSort(forwardIterator first, forwardIterator last, Compare comp) {
+    bool sorted = false;
+    while (!sorted) {
+      sorted = true;
+      for (auto it = first; it != last; ++it) {
+        auto next = it;
+        ++next;
+        if (next != last && comp(*next, *it)) {
+          std::swap(*it, *next);
+          sorted = false;
+        }
+        if (next == last) {
+          break;
+        }
+        ++it;
+      }
+      auto it = first;
+      ++it;
+      for (; it != last; ++it) {
+        auto next = it;
+        ++next;
+        if (next != last && comp(*next, *it)) {
+          std::swap(*it, *next);
+          sorted = false;
+        }
+        if (next == last) {
+          break;
+        }
+        ++it;
+      }
+    }
+  }
+
+  template< typename forwardIterator, typename Compare >
+  void insertionSort(forwardIterator first, forwardIterator last, Compare comp) {
+    if (first == last) {
+      return;
+    }
+    for (auto it = std::next(first); it != last; ++it) {
+      auto key = *it;
+      auto j = it;
+      while (j != first && comp(key, *std::prev(j))) {
+        *j = *std::prev(j);
+        --j;
+      }
+      *j = key;
+    }
+  }
+
+  template< typename T, typename Compare >
+  void stdSort(std::list< T > &lst, Compare comp) {
+    lst.sort(comp);
+  }
+
+  template< typename T >
+  void performSorts(List< T > &singleList, std::list< T > &doubleList, bool ascending, std::ostream &os) {
+    std::function< bool(const T &, const T &) > comp;
+    if (ascending) {
+      comp = std::less< T >();
+    }
+    else {
+      comp = std::greater< T >();
+    }
+
+    printList(singleList, os);
+    oddEvenSort(singleList.begin(), singleList.end(), comp);
+    printList(singleList, os);
+    insertionSort(doubleList.begin(), doubleList.end(), comp);
+    printStdList(doubleList, os);
+    stdSort(doubleList, comp);
+    printStdList(doubleList, os);
+  }
+}
+
+#endif
