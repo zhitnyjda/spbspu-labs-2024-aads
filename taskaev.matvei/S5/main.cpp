401d2f2caf505cd31a4a21e12284316b5f583fa3
amotvei <motya2228@mail.ru>
2024-06-05 19:43:30 +0300

    написал фигню и устал пойду погуляю

--- taskaev.matvei/S5/main.cpp
+++ taskaev.matvei/S5/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "BSTree.hpp"
+using namespace taskaev;
 
 int main(int argc, char ** argv)
 {
   if (argc != 3)
+  {
+    std::cerr << "Error: not open file!\n";
+    return 1;
+  }
+  BSTree< size_t, std::string> tree;
+  std::ifstream inputs(argv[1]);
+  if (!inputs)
   {
     std::cerr << "Error: not open file!\n";
     return 1;
