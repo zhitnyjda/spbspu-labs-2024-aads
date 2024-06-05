819e1db7af6d0706ea7966a7449f2dfcf42f3d91
amotvei <motya2228@mail.ru>
2024-06-05 23:34:57 +0300

    ухх я устал уже надеюсь я не будет 100 комитов и я написал код без утечек памяти

--- taskaev.matvei/S5/functions.cpp
+++ taskaev.matvei/S5/functions.cpp
@@ -157,4 +157,52 @@ namespace taskaev
     }
   }
 
+  void breadth(BSTree< int, std::string >& tree)
+  {
+    if (tree.empty())
+    {
+      errorEmpty(std::cout);
+      return;
+    }
+    else
+    {
+      Queue< std::string > queue;
+      int result = 0;
+      tree.traverse_lnr([&](const std::pair< const int, std::string >& value)
+        {
+          int max = std::numeric_limits<int>::max();
+          int min = -max;
+          if (value.first > 0 && max - value.first < result)
+          {
+            throw;
+          }
+          else if (value.first < 0 && min - value.first > result)
+          {
+            throw;
+          }
+          result = result + value.first;
+          queue.push(value.second);
+        });
+      std::cout << result << " ";
+      while (!queue.isEmpty())
+      {
+        std::cout << queue.front();
+        queue.pop();
+        if (!queue.isEmpty())
+        {
+          std::cout << " ";
+        }
+        else
+        {
+          std::cout << "\n";
+        }
+      }
+    }
+  }
+
+  void errorEmpty(std::ostream& out)
+  {
+    out << "<EMPTY>\n";
+  }
+
 }
--- taskaev.matvei/S5/functions.hpp
+++ taskaev.matvei/S5/functions.hpp
@@ -10,6 +10,8 @@ namespace taskaev
   void createTree(std::istream& in, BSTree<int, std::string>& tree); // взял из S4 и переделал чуть чуть
   void ascending(BSTree< int, std::string >& tree);
   void descending(BSTree< int, std::string >& tree);
+  void breadth(BSTree< int, std::string >& tree);
+  void errorEmpty(std::ostream& out); // может стоило файл притинуть из S4 но мне кажется это было бы слишкомO
 }
 
 #endif
