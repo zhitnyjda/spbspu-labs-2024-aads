582ed3e5fa15edc40fc6a73057a96dacdf0e6618
anyadg <anna_giraffe@mail.ru>
2024-06-05 16:47:50 +0300

    removed duplication

--- doroshenko.anna/S6/main.cpp
+++ doroshenko.anna/S6/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdexcept>
 #include "sort.hpp"
+#include "BinarySearchTree.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -59,5 +60,20 @@ int main(int argc, char* argv[])
     }
   }
 
+  BST< std::string, BST< std::string, std::function< void(std::ostream& stream, std::string type, size_t size) > > > sorts;
+  BST< std::string, std::function< void(std::ostream& stream, std::string type, size_t size) > > ints;
+  BST< std::string, std::function< void(std::ostream& stream, std::string type, size_t size) > > floats;
+
+  using namespace std::placeholders;
+  ints.insert("ascending", std::bind(sortData< int, std::less< int > >, _1, _2, _3, std::less< int >{}));
+  ints.insert("descending", std::bind(sortData< int, std::greater< int > >, _1, _2, _3, std::greater< int >{}));
+
+  floats.insert("ascending", std::bind(sortData< float, std::less< float > >, _1, _2, _3, std::less< float >{}));
+  floats.insert("descending", std::bind(sortData< float, std::greater< float > >, _1, _2, _3, std::greater< float >{}));
+
+  sorts.insert("floats", floats);
+  sorts.insert("ints", ints);
+
+  sorts.at(type).second.at(cmp).second(std::cout, type, size);
   return 0;
 }
