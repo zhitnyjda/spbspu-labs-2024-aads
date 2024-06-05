5feed529cf52c7cc3e92538a49ce756dc11acf3b
oksidmargantsa <sukachva.maria@gmail.com>
2024-06-05 17:00:23 +0300

    I'm desperate

--- sukacheva.maria/S5/KeySum.hpp
+++ sukacheva.maria/S5/KeySum.hpp
@@ -13,11 +13,11 @@ namespace sukacheva
     {
       long long max = std::numeric_limits< long long >::max();
       long long min = std::numeric_limits< long long >::min();
-      if (keyValue.first > 0 && result > max - keyValue.first)
+      if (keyValue.first >= 0 && result >= max - keyValue.first)
       {
         throw std::overflow_error("<OVERFLOW>\n");
       }
-      else if (keyValue.first < 0 && result < min - keyValue.first)
+      else if (keyValue.first <= 0 && result <= min - keyValue.first)
       {
         throw std::underflow_error("<UNDERFLOW>\n");
       }
