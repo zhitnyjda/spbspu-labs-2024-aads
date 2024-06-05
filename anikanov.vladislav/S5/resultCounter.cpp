1884a9427d8a980f637b3da51df6dbcc03a625c8
Anikanov Vlad <anikanovvlad@yandex.ru>
2024-06-05 18:21:49 +0300

    fix print

--- anikanov.vladislav/S5/resultCounter.cpp
+++ anikanov.vladislav/S5/resultCounter.cpp
@@ -19,5 +19,8 @@ int anikanov::ResultCounter::getResult() const
 
 std::string anikanov::ResultCounter::getValues() const
 {
+  if (!values.empty()) {
+    return values.substr(0, values.size() - 1);
+  }
   return values;
 }
