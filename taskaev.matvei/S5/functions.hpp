69e76ec9f0da3118c5a14c8061c45a9cb50b84c5
amotvei <motya2228@mail.ru>
2024-06-05 23:53:24 +0300

    я убрал коменты в программе

--- taskaev.matvei/S5/functions.hpp
+++ taskaev.matvei/S5/functions.hpp
@@ -7,11 +7,11 @@
 
 namespace taskaev
 {
-  void createTree(std::istream& in, BSTree<int, std::string>& tree); // взял из S4 и переделал чуть чуть
+  void createTree(std::istream& in, BSTree<int, std::string>& tree);
   void ascending(BSTree< int, std::string >& tree);
   void descending(BSTree< int, std::string >& tree);
   void breadth(BSTree< int, std::string >& tree);
-  void errorEmpty(std::ostream& out); // может стоило файл притинуть из S4 но мне кажется это было бы слишкомO
+  void errorEmpty(std::ostream& out);
 }
 
 #endif
