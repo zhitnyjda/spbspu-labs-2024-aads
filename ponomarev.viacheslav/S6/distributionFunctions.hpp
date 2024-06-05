0f7d7d57cf254ee25e2016e44f7ca23d454bb3f7
ponomarev.viacheslav <slava_ponomarev_05@mail.ru>
2024-06-05 16:49:11 +0300

    added shaker and Insertion

--- ponomarev.viacheslav/S6/distributionFunctions.hpp
+++ ponomarev.viacheslav/S6/distributionFunctions.hpp
@@ -1,25 +1,26 @@
 #include <deque>
 #include <forward_list>
 #include <algorithm>
-#include "list.hpp"
+#include <list>
+#include "sorts.hpp"
 
 namespace ponomarev
 {
-  void inputRandomData(List< int > & list, int size)
+  void inputRandomData(std::list< int > & list, int size)
   {
     for (int i = 0; i < size; ++i)
     {
-      list.pushBack((rand() % 1000));
+      list.push_front((rand() % 1000));
     }
   }
 
-  void inputRandomData(List< double > & list, int size)
+  void inputRandomData(std::list< double > & list, int size)
   {
     for (int i = 0; i < size; ++i)
     {
       double firstNum = (double(rand() % 1000) - double(rand() % 1000));
       double secondNum = (double(rand() % 100) / double(rand() % 100));
-      list.pushBack(firstNum + secondNum);
+      list.push_front(firstNum + secondNum);
     }
   }
 
@@ -38,32 +39,25 @@ namespace ponomarev
   void makeSort(std::ostream & out, int size, Compare comp)
   {
     std::forward_list< T > forwardList;
-    List< T > firstList;
-    List< T > secondList;
+    std::list< T > firstList;
+    std::list< T > secondList;
     inputRandomData(firstList, size);
     std::deque< T > firstDeque;
     std::deque< T > secondDeque;
     std::deque< T > thirdDeque;
 
-    std::copy(firstList.begin(), firstList.end(), std::back_inserter(firstList));
+    std::copy(firstList.begin(), firstList.end(), std::back_inserter(secondList));
     std::copy(firstList.begin(), firstList.end(), std::back_inserter(firstDeque));
     std::copy(firstList.begin(), firstList.end(), std::back_inserter(secondDeque));
-
-    typename List< T >::Iterator first = firstList.begin();
-    typename List< T >::Iterator last = firstList.end();
-    while (first != last)
-    {
-      secondList.pushBack(*first);
-      first++;
-    }
+    std::copy(firstList.begin(), firstList.end(), std::back_inserter(thirdDeque));
 
     printData(out, firstList.begin(), firstList.end());
 
-    sortShaker(forwardList.begin(), forwardList.end(), comp);
     sortShaker(firstList.begin(), firstList.end(), comp);
     sortInsertion(secondList.begin(), secondList.end(), comp);
     sortShaker(firstDeque.begin(), firstDeque.end(), comp);
     sortInsertion(secondDeque.begin(), secondDeque.end(), comp);
+    forwardList.sort(comp);
     std::sort(thirdDeque.begin(), thirdDeque.end(), comp);
 
     printData(out, forwardList.begin(), forwardList.end());
---
+++ ponomarev.viacheslav/S6/sorts.hpp
@@ -0,0 +1,64 @@
+#ifndef SORTS_HPP
+#define SORTS_HPP
+
+#include <algorithm>
+#include <iostream>
+
+namespace ponomarev
+{
+  template < typename Iterator, typename Comparator >
+  void sortInsertion(Iterator first, Iterator last, Comparator comp)
+  {
+    for (auto it = first; it != last; ++it)
+    {
+      auto current = *it;
+      auto pos = it;
+      while (pos != first && comp(current, *std::prev(pos)))
+      {
+        *pos = *std::prev(pos);
+        if (pos != first) {
+          --pos;
+        }
+      }
+
+      *pos = current;
+    }
+  }
+
+  template < typename Iterator, typename Comparator >
+  void sortShaker(Iterator first, Iterator last, Comparator comp)
+  {
+    last--;
+    while (first != last)
+    {
+      Iterator left = first;
+      Iterator right = last;
+
+      for (auto it = left; it != right; it++)
+      {
+        if (comp(*std::next(left), *left))
+        {
+          std::iter_swap(left, std::next(left));
+          last = left;
+        }
+      }
+
+      if (left == first)
+      {
+        break;
+      }
+
+      for (auto it = right; it != left; it--)
+      {
+        if (comp(*right, *std::prev(right)))
+        {
+          std::iter_swap(right, std::prev(right));
+        }
+      }
+
+      last++;
+    }
+  }
+}
+
+#endif
