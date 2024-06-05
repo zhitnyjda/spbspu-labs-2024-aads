8fa707b22ebc649c14a3d99f511d1b95986c5e01
ponomarev.viacheslav <slava_ponomarev_05@mail.ru>
2024-06-05 15:57:18 +0300

    added filling of all containers, output func

--- ponomarev.viacheslav/S6/distributionFunctions.hpp
+++ ponomarev.viacheslav/S6/distributionFunctions.hpp
@@ -9,7 +9,7 @@ namespace ponomarev
   {
     for (int i = 0; i < size; ++i)
     {
-      list.pushBack((rand() % 1000) - (rand() % 1000));
+      list.pushBack((rand() % 1000));
     }
   }
 
@@ -23,18 +23,32 @@ namespace ponomarev
     }
   }
 
+  template< typename It >
+  std::ostream & printData(std::ostream & out, It first, It last)
+  {
+    while (first != last)
+    {
+      out << *first;
+      out << (++first == last ? '\n' : ' ');
+    }
+    return out;
+  }
+
   template< typename T, typename Compare >
   void makeSort(std::ostream & out, int size, Compare comp)
   {
     std::forward_list< T > forwardList;
     List< T > firstList;
+    List< T > secondList;
     inputRandomData(firstList, size);
     std::deque< T > firstDeque;
     std::deque< T > secondDeque;
-    List< T > secondList;
+    std::deque< T > thirdDeque;
+
     std::copy(firstList.begin(), firstList.end(), std::back_inserter(firstList));
     std::copy(firstList.begin(), firstList.end(), std::back_inserter(firstDeque));
     std::copy(firstList.begin(), firstList.end(), std::back_inserter(secondDeque));
+
     typename List< T >::Iterator first = firstList.begin();
     typename List< T >::Iterator last = firstList.end();
     while (first != last)
@@ -42,5 +56,21 @@ namespace ponomarev
       secondList.pushBack(*first);
       first++;
     }
+
+    printData(out, firstList.begin(), firstList.end());
+
+    sortShaker(forwardList.begin(), forwardList.end(), comp);
+    sortShaker(firstList.begin(), firstList.end(), comp);
+    sortInsertion(secondList.begin(), secondList.end(), comp);
+    sortShaker(firstDeque.begin(), firstDeque.end(), comp);
+    sortInsertion(secondDeque.begin(), secondDeque.end(), comp);
+    std::sort(thirdDeque.begin(), thirdDeque.end(), comp);
+
+    printData(out, forwardList.begin(), forwardList.end());
+    printData(out, firstList.begin(), firstList.end());
+    printData(out, secondList.begin(), secondList.end());
+    printData(out, firstDeque.begin(), firstDeque.end());
+    printData(out, secondDeque.begin(), secondDeque.end());
+    printData(out, thirdDeque.begin(), thirdDeque.end());
   }
 }
