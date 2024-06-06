3e0a851c1de1b0491caa80369907b991d25f3701
oksidmargantsa <sukachva.maria@gmail.com>
2024-06-06 15:14:45 +0300

    work with wrong input

--- sukacheva.maria/S6/main.cpp
+++ sukacheva.maria/S6/main.cpp
@@ -14,41 +14,26 @@ int main(int argc, char* argv[])
   }
   std::string cmp = argv[1];
   std::string type = argv[2];
-  long long size = std::stoll(argv[3]);
-  bool cmpFlagError = false;
-  if (cmp != "ascending" && cmp != "descending")
-  {
-    cmpFlagError = true;
-  }
-  bool typeFlagError = false;
-  if (type != "ints" && type != "floats")
-  {
-    typeFlagError = true;
-  }
-  if (cmpFlagError || typeFlagError || size < 0)
-  {
-    std::cerr << "<WRONG INPUT>\n";
-    return 1;
-  }
-  std::map< std::string, std::map < std::string, std::function< void(std::string type, size_t size, std::ostream& out) > > > sort{};
-  std::map < std::string, std::function< void(std::string type, size_t size, std::ostream& out) > > ints{};
-  std::map < std::string, std::function< void(std::string type, size_t size, std::ostream& out) > > floats{};
-  {
-    using namespace std::placeholders;
-    ints["ascending"] = std::bind(sortConts< int, std::less< int > >, _1, _2, _3, std::less< int >());
-    ints["descending"] = std::bind(sortConts< int, std::greater< int > >, _1, _2, _3, std::greater< int >());
-
-    floats["ascending"] = std::bind(sortConts< float, std::less< float > >, _1, _2, _3, std::less< float >());
-    floats["descending"] = std::bind(sortConts< float, std::greater< float > >, _1, _2, _3, std::greater< float >());
-
-    sort["ints"] = ints;
-    sort["floats"] = floats;
-  }
   try
   {
+    long long size = std::stoll(argv[3]);
+    std::map< std::string, std::map < std::string, std::function< void(std::string type, size_t size, std::ostream& out) > > > sort{};
+    std::map < std::string, std::function< void(std::string type, size_t size, std::ostream& out) > > ints{};
+    std::map < std::string, std::function< void(std::string type, size_t size, std::ostream& out) > > floats{};
+    {
+      using namespace std::placeholders;
+      ints["ascending"] = std::bind(sortConts< int, std::less< int > >, _1, _2, _3, std::less< int >());
+      ints["descending"] = std::bind(sortConts< int, std::greater< int > >, _1, _2, _3, std::greater< int >());
+
+      floats["ascending"] = std::bind(sortConts< float, std::less< float > >, _1, _2, _3, std::less< float >());
+      floats["descending"] = std::bind(sortConts< float, std::greater< float > >, _1, _2, _3, std::greater< float >());
+
+      sort["ints"] = ints;
+      sort["floats"] = floats;
+    }
     sort.at(argv[2]).at(argv[1])(type, size, std::cout);
   }
-  catch (const std::out_of_range&)
+  catch (const std::exception& e)
   {
     ErrorMessage(std::cerr);
     return 1;
